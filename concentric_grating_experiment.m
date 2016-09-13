a2d33246eb8a18640e1b5de90ef62fca54500cbe
Mats van Es <m.vanes@donders.ru.nl>
2016-09-13 15:24:14 +0200

    make different trials, introduce baseline period and no shift trials

--- concentric_grating_experiment.m
+++ concentric_grating_experiment.m
@@ -1,14 +1,13 @@
 function concentric_grating_experiment(fileName, isLive, drawMask)
-% This function is copied from DriftDemo and modified to display an
+% This function is copied from DriftDemo and modified to display a (masked)
 % animated concentric grating moving inward. At random time points, the
 % grating will make a 180 degree phase shift, which has to be reported by
-% the subject.
+% the subject by a button press. 20% of the trials have no phase shift.
 
 % NOTE: costs a lot of memory because all images are made individually
 
 % _________________________________________________________________________
 %
-
 % This is a very simple, bare bones demo on how to do frame animation. For
 % much more efficient ways to draw gratings and gabors, have a look at
 % DriftDemo2, DriftDemo3, DriftDemo4, ProceduralGaborDemo, GarboriumDemo,
@@ -18,12 +17,13 @@ function concentric_grating_experiment(fileName, isLive, drawMask)
 % generated, which is moved every frame to create the illusion of movement.
 % However, this is not possible for a concentric grating, so every frame a
 % new texture has to be generated.
-%
 % _________________________________________________________________________
 
 % HISTORY
 % 7/6/2016: change grating to concentric grating
 % 8/6/2016: add circular and gaussian mask, introduce phase-shift
+% 9/6/2016: make trials, introduce baseline period
+% 10/6/2016: add no shift trials
 
 %% Function settings
 if nargin<1
@@ -43,11 +43,11 @@ if isempty(isLive)
 end
 
 if nargin<3
-    drawMask = 0;
+    drawMask = 1;
 end
 
 if isempty(drawMask)
-    drawMask = 0;
+    drawMask = 1;
 end
 try
     
@@ -65,7 +65,11 @@ try
     % the stimulus display.  Chosing the display with the highest dislay number is
     % a best guess about where you want the stimulus displayed.
     screens=Screen('Screens');
-    screenNumber=max(screens);
+    if isLive
+        screenNumber=max(screens);
+    else
+        screenNumber=2;
+    end
     
     % Find the color values which correspond to white and black: Usually
     % black is always 0 and white 255, but this rule is not true if one of
@@ -90,7 +94,7 @@ try
     
     % Open a double buffered fullscreen window and select a black background
     % color:
-    [window, windowRect]=Screen('OpenWindow',screenNumber, black);
+    [window, windowRect]=Screen('OpenWindow',screenNumber, gray);
     
     % Get the centre coordinate of the window
     [xCenter, yCenter] = RectCenter(windowRect);
@@ -102,24 +106,26 @@ try
     % Set up alpha-blending for smooth (anti-aliased) lines
     Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
     
-    HideCursor;
+%     HideCursor;
     
-    %% Input settings
+    
+    %% Trigger settings
     if isLive
-        btsi = Bitsi('com1'); %when there is no com connected, use keyboard input as default
+        btsi = Bitsi('com1');% when there is no com connected, use keyboard as input
     else
         btsi = Bitsi('');
     end
-    xp = expconstants(); %load bitsi trigger codes
     
-    KbName('UnifyKeyNames');
+    xp = expconstants();
     
-    % user input
-    space = KbName('space');
+    %% Input settings
+    
+    KbName('UnifyKeyNames'); % converts operating specific keynames to universal keyname
     escape = KbName('ESCAPE');
-    
+    space = KbName('space');
     
     %% Experimental settings
+    % calculate the size of the stimulus according to the used setup
     if isLive
         screen.width = 49; %MEG screenwidth in cm
         screen.viewingDistance = 76; % in cm
@@ -130,17 +136,21 @@ try
         screen.resolutionX = 1920; %desktop screen in pixels
     end
     
-    screen.totVisDeg = 2*atan(screen.width/(2*screen.viewingDistance))*(180/pi);
+    screen.totVisDeg = 2*atan(screen.width/(2*screen.viewingDistance))*(180/pi); % formula for calculating visual degrees
     screen.pixPerDeg = screen.resolutionX/screen.totVisDeg;
     
-    visualAngleGrating = 25;
+    visualAngleGrating = 12.5;
     gratingSize = visualAngleGrating*screen.pixPerDeg; % calculate how big the grating should be (can not be rounded off, might result in odd integer)
     gratingRadius = round(gratingSize/2); % the grating can only exist of integers, so round
     gratingSize = 2*gratingRadius; % to prevent consistency errors, redifine gratingSize
     
+    instructions = 'Instructions \n\n Fixate at the middle of the screen. \n\n Press the button if you see a shift in the animation';
     ntrials=200;
-    preCueTime=1.5; %cue can come 1.5 seconds after start trial
-    trialTime = 8.5; % trials take 8.5 seconds. with a driftfreq of 1 sec and a 180 degree phase-shift, the phase at the end of every trial should be the same as the phase at the beginning of the next trial.
+    chanceNoShift = 0.2; % in 20% of the trials, have no shift.
+    preStimTime=2; % fixation, baseline period
+    preShiftTime=1.5; % cue can come 1.5 seconds after start trial
+    shiftRange = 5; % present the shift at random in 5 seconds
+    postShiftTime = 1.5; % keep animation going 1.5 seconds after shift
     driftFreq = 1; % in one second, every pixel of the grating completes one cylce (black-white-black)
     numFrames=round(driftFreq/ifi); % temporal period, in frames, of the drifting grating
     
@@ -148,7 +158,7 @@ try
     [x,y]=meshgrid(-gratingRadius:gratingRadius,-gratingRadius:gratingRadius);
     f=1.5*2*pi; % cycles/pixel
     
-    keyPress = zeros(ntrials,3); %safe shift onset, response and response time
+    keyPress = zeros(ntrials,4); %safe shift onset, response and response time
     
     %% Generate stimulus
     % generate a mask to cover the edges of the grating, making it a circle
@@ -156,122 +166,129 @@ try
     M = zeros(2*gratingRadius+1, 2*gratingRadius+1, 1); %zero matrix same size as the grid
     M(:,:,1) = sqrt(x.^2 + y.^2) < R ; %disk formula: all masked parts of the grid must be zero.
     
-    
     % Generate grating texture
     % Compute each frame of the movie and convert those frames stored in
     % MATLAB matrices, into Psychtoolbox OpenGL textures using 'MakeTexture';
-    
-    %________________________________________________
-    % very inefficient, costs a lot of memory.. alternative?
-    
-    
+    % NOTE: very inefficient, costs a lot of memory.. alternative?
+    tex=zeros(numFrames,1);
+
     for i=1:numFrames
         phase=(i/numFrames)*2*pi; %change the phase of the grating according to the framenumber
         m=sin(sqrt(x.^2+y.^2) / f + phase);
         tex(i)=Screen('MakeTexture', window, M.*(gray+inc*m)); % multiply mask with grating
+        tex(i)=Screen('MakeTexture', window, M.*(gray+inc*m));
     end
     
-    %________________________________________________
-    
-    mask=ones(2*gratingRadius+1, 2*gratingRadius+1, 2) * black;
+    % make hanning mask
+    mask=ones(2*gratingRadius+1, 2*gratingRadius+1, 2) * gray;
     mask(:, :, 2)=white * (1 - exp(-((x/300).^2)-((y/300).^2)));
     masktex=Screen('MakeTexture', window, mask);
     
-    
-    shiftPoint = preCueTime + 6*rand(ntrials,1); % generate a random number between 1.5 and 7.5 seconds for the timing of the phase shift
+    shiftPoint = ((preShiftTime+shiftRange)-preShiftTime).*rand(ntrials,1) + preShiftTime;
+    trlNoShift = sort(randperm(ntrials, ntrials*chanceNoShift)); % generate a number of random (w/o replacement) trials that won't have a phase shift
+    %   shiftPoint(trlNoShift)=0; % replace the noShift trials with 0.
+    %   instead, make sure that these trials won't have a shift (otherwise it
+    %   also means short trial)
     shiftFrame = round(shiftPoint*frameRate);
     keyPress(:,1)=shiftPoint;
+    
+    KbName('UnifyKeyNames'); % convert operating specific keynames to general keynames
+    escape = KbName('ESCAPE');
+    space = KbName('space');
+    
     %% Start Experiment
+    
+    % Run the movie animation for a variable period: end it 1.5 seconds
+    % after phase shift.
+    movieDurationSecs=shiftPoint+postShiftTime;
+    % Convert movieDuration in seconds to duration in frames to draw:
+    movieDurationFrames=round(movieDurationSecs * frameRate);
+    % assign the right texture indices to frames
     for trl=1:ntrials
-        % Run the movie animation for a variable period: end it 1.5 seconds
-        % after phase shift.
-        movieDurationSecs=trialTime;
-        % Convert movieDuration in seconds to duration in frames to draw:
-        movieDurationFrames=round(movieDurationSecs * frameRate);
-        % assign the right texture indices to frames
-        movieFrameIndices=mod(0:(movieDurationFrames-1), numFrames) + 1;
+        if trl==1
+            % Provide instructions at the beginning of the experiment
+            Screen('FillRect', window, gray);
+            DrawFormattedText(window, instructions,...
+                'center', 'center', white, 55, [], [], 1.3);
+            Screen('Flip', window);
+            KbWait; % wait until key is pressed
+            pause(0.5)
+        end
+        movieFrameIndices=mod(0:(movieDurationFrames(trl)-1), numFrames) + 1; %assign the right image index to each frame
+        
         
         % Use realtime priority for better timing precision:
         priorityLevel=MaxPriority(window);
         Priority(priorityLevel);
         
+        % Baseline period, black screen with fixation dot
+        Screen('DrawDots', window, [xCenter yCenter], 20, [white white white], [], 2);
+        Screen('Flip', window)
+        pause(preStimTime)
+        tic % measure time
         
+        btsi.sendTrigger(xp.TRIG_ONSET_TRIAL);
         % Animation loop:
-        %     tic
-        btsi.sendTrigger(xp.TRIG_ONSET_TRIAL);% send bitsi trigger
-        t0=GetSecs;
-        for i=1:movieDurationFrames
+        for i=1:movieDurationFrames(trl)
+            if i==shiftFrame(trl)
+                t0=GetSecs; % get time stamp at the moment of phase shift in order to calculate reaction time
+            end
+            
             % at the moment the phase-shift should occur, jump to the texture
             % with a 180 degree phase shift, and add this shift for the
             % remainder of the video. Otherwise, don't shift.
-            if i==shiftFrame(trl)
-                btsi.sendTrigger(xp.TRIG_SHIFT);
-            end
-            
-            if i>=shiftFrame(trl) && movieFrameIndices(trl)>numFrames/2
+            if i>=shiftFrame(trl) && movieFrameIndices(i)>numFrames/2 && ~any(trlNoShift==trl)
                 shift = -numFrames/2; % if more than half of the period already passed, subtract the shift
                 % enable abort using esc
-                [keyIsDown, secs, keyCode] = KbCheck(-3);
-                if keyCode(escape)
-                    save(fileName, 'keyPress'); % save as .mat
-                    sca;
-                    break;
-                end
-            elseif i>=shiftFrame(trl) && movieFrameIndices(trl)<=numFrames/2
+            elseif i>=shiftFrame(trl) && movieFrameIndices(i)<=numFrames/2 && ~any(trlNoShift==trl)
                 shift = numFrames/2; % otherwise add the shift (because the period loops)
-                [keyIsDown, secs, keyCode] = KbCheck(-3);
-                if keyCode(escape)
-                    save(fileName, 'keyPress'); % save as .mat
-                    sca;
-                    break;
-                end
             else
                 shift=0;
             end
             
+            % Draw the appropriate image
             Screen('DrawTexture', window, tex(movieFrameIndices(i) + shift));
             
+            % Draw the Gaussian mask if required
             if drawMask
                 Screen('DrawTexture', window, masktex);
             end
+            
             % Show it at next display vertical retrace. Please check DriftDemo2
             % and later, as well as DriftWaitDemo for much better approaches to
             % guarantee a robust and constant animation display timing! This is
             % very basic and not best practice!
+            if i==shiftFrame(trl);
+                btsi.sendTrigger(xp.TRIG_SHIFT);
+            end
             Screen('Flip', window);
-        end
-        %     toc
-        btsi.sendTrigger(xp.TRIG_OFFSET_TRIAL);
-        if isLive
-            btsi.clearResponses();
-            [resp, rt] = btsi.getResponse(respondTime, true);
-            if resp == 'h'
-                % left red button down event
-                keyPress(trl,1) = 2;
-            else
-                keyPress(trl,1) = 0;
-            end
-            keyPress(trl,3) = rt;
-        else
             [keyIsDown, secs, keyCode] = KbCheck(-3);
-            % % Use -1 to listen to all keyboards
-            % % Use -2 to listen to all keypad devices
-            % % Use -3 to listen to all keyboards and keypads
-            while keyCode(space)==0 && secs-t0>=shiftPoint %while function does not stop from going to next trial
-                [keyIsDown, secs, keyCode] = KbCheck(-3);
+            %% How to make sure only the first response is saved?
+            %             while keyCode(space)==0
+            %                 [keyIsDown, secs, keyCode] = KbCheck(-3); % check whether a button was pressed; only record first button press
+            %             end
+            if keyCode(escape) %if escape is pressed
+                save(fileName, 'keyPress', 'trlNoShift') % save the data
+                sca % close the screen
+                break % and end PTB
+            elseif keyCode(space) % if space is pressed
+                if i>=shiftFrame(trl)
+                    keyPress(trl,2)=1; % report
+                    keyPress(trl,3) = secs-t0; % report reaction time
+                else
+                    keyPress(trl,2)=2; % if space is pressed before phase shift, report it.
+                end
             end
-            if keyCode(space)
-                keyPress(k,1) = 1;
-            else
-                keyPress(k,1) = 0;
-            end
-            keyPress(k,2) = secs-t0; % provide reaction time t(keypress) - t(present rotating grating)
+            
         end
+        btsi.sendTrigger(xp.TRIG_OFFSET_TRIAL);
+        keyPress(trl,4)=toc-postShiftTime;% the amount of time it takes for the animation
+        % keyPress(:,1) is the amount of time it should take to run the
+        % animation.
         Priority(0);
     end
+    save(fileName, 'keyPress', 'trlNoShift')
     
-    % Export data
-    save(fileName, 'keyPress'); % save as .mat
     
     % Close all textures. This is not strictly needed, as
     % Screen('CloseAll') would do it anyway. However, it avoids warnings by
