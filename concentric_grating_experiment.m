bd4c02b27f22e4533ed98c464934233716c92eae
Mats van Es <m.vanes@donders.ru.nl>
2016-09-13 15:33:19 +0200

    change naming convention; delete offset triggers; edit bitsi button response

--- concentric_grating_experiment.m
+++ concentric_grating_experiment.m
@@ -28,7 +28,7 @@ function concentric_grating_experiment(fileName, isLive)
 % 16/6/2016: edit Gaussian mask, add (rectangular) hanning mask
 % 16/6/2016: add circular hanning mask
 % 17/6/2016: debugged circular hanning mask and response buttons
-% 20/6/2016: change location to lower visual field, left and right.
+% 20/6/2016: change location to lower visual field, lef t and right.
 % 21/6/2016: edit registereing of user input, record timing, edit locations
 %   of gratings (central, 2 peripheral) and introduce trial
 %   counterbalancing (semirandom instead of random locations)
@@ -39,6 +39,8 @@ function concentric_grating_experiment(fileName, isLive)
 %   moved to the [black, white] interval.
 %   solved bug in saving response
 % 22/6/2016: introduce Bitsi responses, solve bugs in saving responses.
+% 23/6/2016: changed naming convention
+% 28/6/2016: deleted offset trigger, edited bitsi button response
 
 %% Function settings
 
@@ -61,11 +63,11 @@ end
 
 try
     %% Standard PTB settings
-    % This script calls Psychtoolbox commands available only in OpenGL-based
-    % versions of the Psychtoolbox. (So far, the OS X Psychtoolbox is the
-    % only OpenGL-base Psychtoolbox.)  The Psychtoolbox command AssertPsychOpenGL will issue
-    % an error message if someone tries to execute this script on a computer without
-    % an OpenGL Psychtoolbox
+    % This script calls Psychtoolbox commands available only in OpenGL-
+    % based versions of the Psychtoolbox. (So far, the OS X Psychtoolbox is
+    % the only OpenGL-base Psychtoolbox.)  The Psychtoolbox command 
+    % AssertPsychOpenGL will issue an error message if someone tries to 
+    % execute this script on a computer without an OpenGL Psychtoolbox
     AssertOpenGL;
     
     % Get the list of screens and choose the one with the highest screen number.
@@ -75,9 +77,9 @@ try
     % a best guess about where you want the stimulus displayed.
     screens=Screen('Screens');
     if isLive
-        screenNumber=max(screens);
+        screenNo=max(screens);
     else
-        screenNumber=1;
+        screenNo=1;
     end
     
     % Find the color values which correspond to white and black: Usually
@@ -85,8 +87,8 @@ try
     % the high precision framebuffer modes is enabled via the
     % PsychImaging() commmand, so we query the true values via the
     % functions WhiteIndex and BlackIndex:
-    white=WhiteIndex(screenNumber);
-    black=BlackIndex(screenNumber);
+    white=WhiteIndex(screenNo);
+    black=BlackIndex(screenNo);
     
     % Round gray to integral number, to avoid roundoff artifacts with some
     % graphics cards:
@@ -103,14 +105,14 @@ try
     
     % Open a double buffered fullscreen window and select a black background
     % color:
-    [window, windowRect]=Screen('OpenWindow',screenNumber, gray);
+    [window, windowRect]=Screen('OpenWindow',screenNo, gray);
     
     % Get the centre coordinate of the window
     [xCenter, yCenter] = RectCenter(windowRect);
     
     % Query the frame duration
     ifi = Screen('GetFlipInterval', window);
-    frameRate=Screen('FrameRate',screenNumber); %1/ifi
+    frameRate=Screen('FrameRate',screenNo); %1/ifi
     
     % Set up alpha-blending for smooth (anti-aliased) lines
     Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
@@ -139,7 +141,7 @@ try
     %% Experimental settings
     % calculate the size of the stimulus according to the used setup
     if isLive
-        screen.width = 49; %MEG screenwidth in cm
+        screen.width = 52; %MEG screenwidth in cm
         screen.viewingDistance = 76; % in cm
     else
         screen.width = 52;%desktop screenwidth in cm
@@ -153,26 +155,28 @@ try
     
     visualAngleGrating = 12.5;
     visualAngleLocation = 15;
-    gratingSize = visualAngleGrating*screen.pixPerDeg; % calculate how big the grating should be (can not be rounded off, might result in odd integer)
+    gratingSize = visualAngleGrating*screen.pixPerDeg; % calculate how big 
+    % the grating should be (can not be rounded off, might result in odd integer)
     gratingRadius = round(gratingSize/2); % the grating can only exist of integers, so round
     gratingSize = 2*gratingRadius; % to prevent consistency errors, redifine gratingSize
     rLocation = round(visualAngleLocation*screen.pixPerDeg/2);
     
-    instructions = 'Instructions \n\n Fixate at the middle of the screen. \n\n Press the button if you see a shift in the animation';
-    ntrialsPerCondition = 150;
+    instructions = 'Instructions \n\n Fixate at the middle of the screen. \n\n Press the button if you see a flash in the animation';
+    nTrialsPerCondition = 150;
     nConditions=3;
-    ntrials=nConditions*ntrialsPerCondition;
-    conditions = [-ones(ntrialsPerCondition,1); zeros(ntrialsPerCondition,1); ones(ntrialsPerCondition,1)];
+    nTrials=nConditions*nTrialsPerCondition;
+    conditions = [-ones(nTrialsPerCondition,1); zeros(nTrialsPerCondition,1);...
+        ones(nTrialsPerCondition,1)];
     conditions = conditions(randperm(size(conditions,1)),:); % shuffle conditions
     
     chanceNoShift = 0.1; % in 20% of the trials, have no shift.
     preStimTime=1.5; % fixation, baseline period
     preShiftTime=1; % cue can come 1 seconds after start trial
-    shiftRange = 2; % present the shift at random in 5 seconds
-    postShiftTime = 1; % keep animation going 1.5 seconds after shift
+    shiftRange = 2; % present the shift at random in 2 seconds
+    postShiftTime = 0.75; % keep animation going 1 seconds after shift
     driftFreq = 1; % in one second, every pixel of the grating completes one cylce (black-white-black)
-    maxRespTime = 0.9; % maximum response time after shift
-    numFrames=round(driftFreq/ifi); % temporal period, in frames, of the drifting grating
+    maxRespTime = 0.7; % maximum response time after shift
+    nFramesInCycle=round(driftFreq/ifi); % temporal period, in frames, of the drifting grating
     
     % settings grating
     [x,y]=meshgrid(-gratingRadius:gratingRadius,-gratingRadius:gratingRadius);
@@ -195,35 +199,36 @@ try
     % Compute each frame of the movie and convert those frames stored in
     % MATLAB matrices, into Psychtoolbox OpenGL textures using 'MakeTexture';
     % NOTE: very inefficient, costs a lot of memory.. alternative?
-    tex=zeros(numFrames,1);
+    tex=zeros(nFramesInCycle,1);
     
-    for i=1:numFrames
-        phase=(i/numFrames)*2*pi; %change the phase of the grating according to the framenumber
+    for jFrame=1:nFramesInCycle
+        phase=(jFrame/nFramesInCycle)*2*pi; %change the phase of the grating according to the framenumber
         m=sin(sqrt(x.^2+y.^2) / f + phase);
         grating = (w2D.*(inc*m)+gray);
         % inc*m fluctuates from [-gray, gray]. Multiply this with the
         % hanning mask to let the grating die off at 0. Now add gray to let
         % the grating fluctuate from [black, white], converging at gray.
-        tex(i)=Screen('MakeTexture', window, grating); % multiply mask with grating
+        tex(jFrame)=Screen('MakeTexture', window, grating); % multiply mask with grating
     end
     
     % set location gratings
     gratingDim = [0 0 2*gratingRadius 2*gratingRadius];
-    gratingYpos = zeros(ntrials,1);
-    for trl=1:ntrials % gratings in periphery are presented in lower visual
+    gratingYpos = zeros(nTrials,1);
+    for iTrl=1:nTrials % gratings in periphery are presented in lower visual
         % field, central stimuli are central.
-        if conditions(trl,1)==1 || conditions(trl,1)==-1
-            gratingYpos(trl,1) = yCenter+0.25*screen.resolutionY;
+        if conditions(iTrl,1)==1 || conditions(iTrl,1)==-1
+            gratingYpos(iTrl,1) = yCenter+0.25*screen.resolutionY;
         else
-            gratingYpos(trl,1) = yCenter;
+            gratingYpos(iTrl,1) = yCenter;
         end
     end
     gratingXpos = xCenter + conditions*rLocation;
     log.Xpos = conditions;
     
-    shiftLatency = ((preShiftTime+shiftRange)-preShiftTime).*rand(ntrials,1) + preShiftTime; % generate latencies at which a shift occurs for every trial
+    shiftLatency = ((preShiftTime+shiftRange)-preShiftTime).*rand(nTrials,1)...
+        + preShiftTime; % generate latencies at which a shift occurs for every trial
     
-    trlNoShift = sort(randperm(ntrials, ntrials*chanceNoShift)); % generate a number of random (w/o replacement) trials that won't have a phase shift
+    trlNoShift = sort(randperm(nTrials, nTrials*chanceNoShift)); % generate a number of random (w/o replacement) trials that won't have a phase shift
     shiftFrame = round(shiftLatency*frameRate);
     log.trlNoShift = trlNoShift;
     
@@ -237,10 +242,10 @@ try
     % after phase shift.
     movieDurationSecs=shiftLatency+postShiftTime;
     % Convert movieDuration in seconds to duration in frames to draw:
-    movieDurationFrames=round(movieDurationSecs * frameRate);
+    nFramesTotal=round(movieDurationSecs * frameRate);
     % assign the right texture indices to frames
-    for trl=1:ntrials
-        if trl==1
+    for iTrl=1:nTrials
+        if iTrl==1
             % Provide instructions at the beginning of the experiment
             Screen('FillRect', window, gray);
             DrawFormattedText(window, instructions,...
@@ -249,9 +254,10 @@ try
             KbWait; % wait until key is pressed
             pause(0.5)
         end
-        movieFrameIndices=mod(0:(movieDurationFrames(trl)-1), numFrames) + 1; %assign the right texture index to each frame
+        frameTexId=mod(0:(nFramesTotal(iTrl)-1), nFramesInCycle) + 1; %assign the right texture index to each frame
         
-        position = CenterRectOnPointd(gratingDim, gratingXpos(trl), gratingYpos(trl)); %move the object of size gratingDim to those coordinates
+        position = CenterRectOnPointd(gratingDim, gratingXpos(iTrl), ...
+                    gratingYpos(iTrl)); %move the object of size gratingDim to those coordinates
         
         
         % Use realtime priority for better timing precision:
@@ -260,8 +266,8 @@ try
         
         % Baseline period, gray screen with fixation dot
         Screen('DrawDots', window, [xCenter yCenter], 20, [255 0 0], [], 2);
-        Screen('Flip', window)
         btsi.sendTrigger(xp.TRIG_ONSET_TRIAL);
+        Screen('Flip', window)
         pause(preStimTime)
         
         [~, ~, keyCode] = KbCheck(-3); % all keyCodes should be zero at start of trial.
@@ -271,26 +277,32 @@ try
         t0= GetSecs; % time onset grating
         
         % Animation loop:
-        for i=1:movieDurationFrames(trl)
+        for jFrame=1:nFramesTotal(iTrl)
             
-            if i==shiftFrame(trl)
+            isShiftFrame = (jFrame==shiftFrame(iTrl));
+            if isShiftFrame
                 t1=GetSecs; % get time stamp at the moment of phase shift in order to calculate reaction time
             end
             
             % at the moment the phase-shift should occur, jump to the texture
             % with a 180 degree phase shift, and add this shift for the
             % remainder of the video. Otherwise, don't shift.
-            if i>=shiftFrame(trl) && movieFrameIndices(i)>numFrames/2 && ~any(trlNoShift==trl)
-                shift = -numFrames/2; % if more than half of the period already passed, subtract the shift
+            isPostShift = (jFrame>=shiftFrame(iTrl));
+            isPostHalfCycle = (frameTexId(jFrame)>nFramesInCycle/2);
+            isPreHalfCycle = (frameTexId(jFrame)<=nFramesInCycle/2);
+            isNoShiftTrl = any(trlNoShift==iTrl);
+            if isPostShift && isPostHalfCycle && ~isNoShiftTrl
+                shift = -nFramesInCycle/2; % if more than half of the period already passed, subtract the shift
                 % enable abort using esc
-            elseif i>=shiftFrame(trl) && movieFrameIndices(i)<=numFrames/2 && ~any(trlNoShift==trl)
-                shift = numFrames/2; % otherwise add the shift (because the period loops)
+            elseif isPostShift && isPreHalfCycle && ~isNoShiftTrl
+                shift = nFramesInCycle/2; % otherwise add the shift (because the period loops)
             else
                 shift=0;
             end
             
             % Draw the appropriate image
-            Screen('DrawTexture', window, tex(movieFrameIndices(i) + shift), [], position);
+            Screen('DrawTexture', window, tex(frameTexId(jFrame) + shift),...
+                [], position);
             Screen('DrawDots', window, [xCenter yCenter], 20, [255 0 0], [], 2);
             
             % NOTE
@@ -299,7 +311,7 @@ try
             % guarantee a robust and constant animation display timing! This is
             % very basic and not best practice!
             
-            if i==shiftFrame(trl);
+            if isShiftFrame;
                 btsi.sendTrigger(xp.TRIG_SHIFT);
             end
             Screen('Flip', window); %% Flipping the screen takes up to 30ms every frame.. 1.8s every cycle..
@@ -322,42 +334,47 @@ try
                 Screen('CloseAll')
             end
             
+            isWithinResponseTime = (jFrame/frameRate<(shiftLatency(iTrl)...
+                                   + maxRespTime));
             if isLive
-                [resp, secs] = btsi.getResponse(respondTime, true);
+                [resp, secs] = btsi.getResponse(0.0001, true);
+                % ...getResponse(x,[]), x should be as small as possible,
+                % because this step will wait maximal x seconds or until
+                % there is a response.
                 if resp == 'd' ...% right red button down event (left button down event is 'h')
-                        && ~flag.prevResp &&  i/frameRate<(shiftLatency(trl) + maxRespTime) % no previous response and within response time
-                    if i>shiftFrame(trl) % response only correct after shift
-                        log.response(trl) = 1;
-                        log.responseTime(trl) = secs-t1;
+                        && ~flag.prevResp && isWithinResponseTime % no previous response and within response time
+                    if jFrame>shiftFrame(iTrl) % response only correct after shift
+                        log.response(iTrl) = 1;
+                        log.responseTime(iTrl) = secs-t1;
                     else % response before shift: incorrect trial
-                        log.response(trl) = 2;
-                        log.responseTime(trl) = 0;
+                        log.response(iTrl) = 2;
+                        log.responseTime(iTrl) = 0;
                     end
-                elseif i==movieDurationFrames(trl) && ~flag.prevResp
-                    log.response(trl) = 0;
-                    log.responseTime(trl)=0;
+                elseif jFrame==nFramesTotal(iTrl) && ~flag.prevResp
+                    log.response(iTrl) = 0;
+                    log.responseTime(iTrl)=0;
                 end
             else
                 [~, secs, keyCode] = KbCheck(-3);
-                if keyCode(space) && ~flag.prevResp &&  i/frameRate<(shiftLatency(trl) + maxRespTime); % no previous response and within response time
-                    if i>shiftFrame(trl) % response only correct after shift
-                        log.response(trl) = 1;
-                        log.responseTime(trl) = secs-t1;
+                if keyCode(space) && ~flag.prevResp &&  isWithinResponseTime % no previous response and within response time
+                    if jFrame>shiftFrame(iTrl) % response only correct after shift
+                        log.response(iTrl) = 1;
+                        log.responseTime(iTrl) = secs-t1;
                     else % response before shift: incorrect trial
-                        log.response(trl) = 2;
-                        log.responseTime(trl) = 0;
+                        log.response(iTrl) = 2;
+                        log.responseTime(iTrl) = 0;
                     end
-                elseif i==movieDurationFrames(trl) && ~flag.prevResp
-                    log.response(trl) = 0;
-                    log.responseTime(trl)=0;
+                elseif jFrame==nFramesTotal(iTrl) && ~flag.prevResp
+                    log.response(iTrl) = 0;
+                    log.responseTime(iTrl)=0;
                 end
-            end
-        end
-        btsi.sendTrigger(xp.TRIG_OFFSET_TRIAL);
-        log.setDuration(trl) = shiftLatency(trl); % set duration till shift
-        log.realDuration(trl) = t1-t0; % real (measured) duration till shift
+            end % stop looking for user input
+        end % end frame presentation
+        log.setDuration(iTrl) = shiftLatency(iTrl); % set duration till shift
+        log.realDuration(iTrl) = t1-t0; % real (measured) duration till shift
+        log.completeDuration(iTrl) = shiftLatency(iTrl) + postShiftTime;
         Priority(0);
-    end
+    end % end trial
     save(fileName, 'log')
     
     
