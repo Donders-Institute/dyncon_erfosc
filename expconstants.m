312c702d271b29c4c88ef7ddc3f148f42885c359
Mats van Es <m.vanes@donders.ru.nl>
2016-09-13 16:04:11 +0200

    edit triggers

--- expconstants.m
+++ expconstants.m
@@ -1,9 +1,9 @@
 function xp = expconstants
 
 xp.TRIG_ONSET_BLINK = 1;
-
-xp.TRIG_ONSET_GRATING = 2;
-xp.TRIG_SHIFT = 3;
-xp.TRIG_RESPONSE = 4;
+xp.TRIG_ONSET_BASELINE = 2;
+xp.TRIG_ONSET_GRATING = 3;
+xp.TRIG_SHIFT = 4;
+xp.TRIG_RESPONSE = 5;
 
 end
