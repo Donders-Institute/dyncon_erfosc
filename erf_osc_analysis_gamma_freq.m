c3f3123fa29d5d8541a4a96cb7841911d8f53888
Mats van Es <m.vanes@donders.ru.nl>
2016-09-30 09:18:35 +0200

    estimate gamma peak frequency

---
+++ erf_osc_analysis_gamma_freq.m
@@ -0,0 +1,101 @@
+function erf_osc_analysis_gamma_freq(subj, isPilot)
+% 
+% trialinfo columns:
+% 1: trialnumber
+% 2: position (-1=left, 0=middle, 1=right)
+% 3: sample of baseline onset
+% 4: sample of grating onset
+% 5: sample of grating shift (=0 if no shift)
+% 6: sample of response (=0 if no response or if response too early)
+if nargin<1
+    subj = 3;
+end
+if isempty(subj)
+    subj = 3;
+end
+if nargin<2
+    isPilot = true;
+end
+if isempty(isPilot);
+    isPilot = true;
+end
+
+%% initiate diary
+workSpace = whos;
+diary('tmpDiary') % save command window output
+fname = mfilename('fullpath')
+datetime
+
+fid = fopen(fullfile([fname '.m']));
+tline = fgets(fid); % returns first line of fid
+while ischar(tline) % at the end of the script tline=-1
+    disp(tline) % display tline
+    tline = fgets(fid); % returns the next line of fid
+end
+fclose(fid);
+
+for i = 1:numel(workSpace) % list all workspace variables
+    workSpace(i).name % list the variable name
+    printstruct(eval(workSpace(i).name)) % show its value(s)
+end
+
+%% load data
+erf_osc_datainfo;
+if isPilot
+    data = load(sprintf('/home/electromag/matves/Data/ERF_oscillation/clean_data/pilot/%02d/cleandata.mat', subj), 'dataClean');
+    load(pilotsubjects(subj).logfile);% load log file
+else
+    data = load(sprintf('/home/electromag/matves/Data/ERF_oscillation/clean_data/experiment/%02d/cleandata.mat', subj), 'dataClean');
+    load(subjects(subj).logfile);% load log file
+end
+data = data.dataClean;
+fs = data.fsample;
+
+% select only shift trials, with valid response
+idxM = find(data.trialinfo(:,5)>0 & data.trialinfo(:,6)>0 & data.trialinfo(:,2)==0);
+nTrials = length(idxM);
+
+cfg=[];
+cfg.trials = idxM(1:nTrials);
+data = ft_selectdata(cfg, data);
+
+
+%% FFT, powerspectrum
+cfg=[];
+cfg.channel = {'MZO', 'MZP', 'MLO', 'MLP', 'MRO', 'MRP'};
+cfg.latency = [-1+1/fs 0];
+dataBaseline = ft_selectdata(cfg, data);
+cfg.latency = [0.4+1/fs 3.75]; % take active time window after first erfs
+dataActive  = ft_selectdata(cfg, data);
+
+cfg=[];
+cfg.foilim = [30 100];
+cfg.method = 'mtmfft';
+cfg.output = 'pow';
+cfg.tapsmofrq = 1;
+cfg.taper = 'dpss';
+cfg.keeptrials = 'no';
+cfg.pad = 2;
+powActive = ft_freqanalysis(cfg, dataActive);
+powBaseline = ft_freqanalysis(cfg, dataBaseline);
+
+cfg=[];
+cfg.operation = '(x1-x2)./x2';
+cfg.parameter = 'powspctrm';
+powDiff = ft_math(cfg, powActive, powBaseline);
+
+% average over channels, take the freq with max gamma pow diff
+gammaAvg = mean(powDiff.powspctrm,1);
+[maxP maxIdx] = max(gammaAvg);
+peakFreq = powDiff.freq(maxIdx);
+
+
+%% save
+filename = sprintf('/home/electromag/matves/Results/ERF_oscillation/freq/%02d/gamma_peak_%d', subj, subj);
+save(fullfile([filename '.mat']), 'peakFreq', 'gammaAvg');
+diary off
+movefile('tmpDiary', fullfile([filename '.txt']));
+
+
+end
+
