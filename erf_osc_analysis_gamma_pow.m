1aa84baf360336a1994edce23f66c1939430aaf8
Mats van Es <m.vanes@donders.ru.nl>
2016-10-10 15:57:36 +0200

    calculation of a virtual channel based on max gamma power is now done in function erf_osc_analysis_gamma_virtualchan. The current function estimates trial-to-trial gamma power based on the virtual channel data

--- erf_osc_analysis_gamma_pow.m
+++ erf_osc_analysis_gamma_pow.m
@@ -1,5 +1,6 @@
 function erf_osc_analysis_gamma_pow(subj, isPilot)
-% This function estimates gamma power at the estimated gamma peak frequency 
+% This function estimates gamma power at the virtual channel (where gamma
+% power was highest at gamma peak frequency)
 
 if nargin<1
     subj = 3;
@@ -35,165 +36,33 @@ end
 
 %% load data
 erf_osc_datainfo;
-if isPilot
-    data = load(sprintf('/home/electromag/matves/Data/ERF_oscillation/clean_data/pilot/%02d/cleandata.mat', subj), 'dataClean');
-    load(pilotsubjects(subj).logfile);% load log file
-else
-    data = load(sprintf('/home/electromag/matves/Data/ERF_oscillation/clean_data/experiment/%02d/cleandata.mat', subj), 'dataClean');
-    load(subjects(subj).logfile);% load log file
-end
-load(sprintf('/home/electromag/matves/Results/ERF_oscillation/freq/%02d/gamma_peak_%d', subj, subj), 'peakFreq');
-data = data.dataClean;
-cfg         = [];
-cfg.channel = 'MEG';
-data        = ft_selectdata(cfg, data);
+load(sprintf('/home/electromag/matves/Results/ERF_oscillation/freq/%02d/gamma_virtual_channel_%d.mat', subj, subj), 'gamPowData');
 
-% select only shift trials, with valid response
-idxM = find(data.trialinfo(:,5)>0 & data.trialinfo(:,6)>0 & data.trialinfo(:,2)==0);
-nTrials = length(idxM);
-
-cfg        = [];
-cfg.trials = idxM(1:nTrials);
-data       = ft_selectdata(cfg, data);
-
-cfg        = [];
-cfg.offset = -(data.trialinfo(:,5)-data.trialinfo(:,4));
-dataShift  = ft_redefinetrial(cfg, data);
-
-cfg            = [];
-cfg.resamplefs = 200;
-data           = ft_resampledata(cfg, data);
-dataShift      = ft_resampledata(cfg, dataShift);
-fs = data.fsample;
-
-
-cfg         = [];
-% cfg.channel = {'MZO', 'MZP', 'MLO', 'MLP', 'MRO', 'MRP'};
+%% gamma power
+cfg=[];
+cfg.offset = -(gam_pow_data.trialinfo(:,5)-gam_pow_data.trialinfo(:,4));
+gam_pow_data_shift = ft_redefinetrial(cfg, gam_pow_data);
+cfg=[];
 cfg.latency = [-1+1/fs 0];
-dataPre     = ft_selectdata(cfg, data);
-% take second preceding shift (NOTE: is it confounding if this includes
-% grating onset, which has higher gamma peak freq?)
-dataPost    = ft_selectdata(cfg, dataShift);
-dataAll     = ft_appenddata([], dataPost, dataPre);
+tmpPre = ft_selectdata(cfg, gam_pow_data);
+tmpPost = ft_selectdata(cfg, gam_pow_data_shift);
 
-%% Frequency analysis
-% calculate power in complete data (for calculation of common filter)
 cfg = [];
-cfg.method    = 'mtmfft';
-cfg.output    = 'powandcsd';
-cfg.tapsmofrq = 5;
-cfg.foilim    = [peakFreq peakFreq];
-freqAll       = ft_freqanalysis(cfg, dataAll);
+cfg.method      = 'mtmfft';
+cfg.output      = 'pow';
+cfg.tapsmofrq   = 5;
+cfg.foilim      = [peakFreq peakFreq];
+cfg.keeptrials  = 'yes';
+gamPowPre      = ft_freqanalysis(cfg, tmpPre);
+gamPowPost     = ft_freqanalysis(cfg, tmpPost);
 
-% calculate power pre and post stimulus 
-cfg = [];
-cfg.method    = 'mtmfft';
-cfg.output    = 'powandcsd';
-cfg.tapsmofrq = 5;
-cfg.foilim    = [peakFreq peakFreq];
-freqPre       = ft_freqanalysis(cfg, dataPre);
-freqPost      = ft_freqanalysis(cfg, dataPost);
+gamPowDif = gamPowPre;
+gamPowDif.powspctrm = (gamPowPost.powspctrm-gamPowPre.powspctrm)./gamPowPre.powspctrm;
 
-%% Source analysis
-mri = ft_read_mri(pilotsubjects(3).mri);
-polhemus = ft_read_headshape(pilotsubjects(3).headshape);
-
-% align mri with MEG data through polhemus headshape
-cfg                         = [];
-cfg.coordsys                = 'ctf';
-cfg.parameter               = 'anatomy';
-cfg.viewresult              = 'yes';
-cfg.method                  = 'headshape';
-cfg.headshape.headshape     = polhemus;
-cfg.headshape.interactive   = 'no';%'yes';
-cfg.headshape.icp           = 'yes';
-mri                         = ft_volumerealign(cfg, mri);
-
-% segment mri
-cfg             = [];
-cfg.write       = 'no';
-cfg.coordsys    = 'neuromag';
-[segmentedmri]  = ft_volumesegment(cfg, mri);
-
-% constructs a volume conduction model from the geometry of the head.
-cfg         = [];
-cfg.method  = 'singleshell';
-headmodel   = ft_prepare_headmodel(cfg, segmentedmri);
-headmodel   = ft_convert_units(headmodel, 'cm');
-
-% constructs a source model, for example a 3-D grid or a cortical sheet.
-cfg                 = [];
-cfg.grid.resolution = 0.5;
-cfg.grid.unit       = 'cm';
-cfg.headmodel       = headmodel;
-cfg.grid            = data.grid;
-sourcemodel         = ft_prepare_sourcemodel(cfg);
-
-% computes the forward model for many dipole locations on a regular 2D or 
-% 3D grid and stores it for efficient inverse modelling
-cfg                 = [];
-cfg.grad            = data.grad;
-cfg.headmodel       = headmodel;
-cfg.reducerank      = 2;
-cfg.grid.resolution = 0.5;   % use a 3-D grid with a 1 cm resolution
-cfg.grid.unit       = 'cm';
-sourcemodel_lf      = ft_prepare_leadfield(cfg);
-
-% calculate common filter
-cfg                   = [];
-cfg.grad              = freqAll.grad;
-cfg.method            = 'dics';
-cfg.frequency         = peakFreq;
-cfg.grid              = sourcemodel_lf;
-cfg.headmodel         = headmodel;
-cfg.dics.projectnoise = 'yes';
-cfg.dics.lambda       = '5%';
-cfg.dics.keepfilter   = 'yes';
-cfg.dics.realfilter   = 'yes';
-sourceAll             = ft_sourceanalysis(cfg, freqAll);
-
-cfg.grid.filter       = sourceAll.avg.filter;
-sourcePre             = ft_sourceanalysis(cfg, freqPre );
-sourcePost            = ft_sourceanalysis(cfg, freqPost);
-sourceDiff            = sourcePost;
-sourceDiff.avg.pow    = (sourcePost.avg.pow - sourcePre.avg.pow) ./ sourcePre.avg.pow;
-
-%% Create virtual channel of maximal gamma power
-% the following position contains the max gamma power difference
-[maxval, maxpowindx] = max(sourceDiff.avg.pow);
-sourceDiff.pos(maxpowindx, :)
-% we will create a virtual channel based on this location. In order to do
-% this, we have to do timelockanalysis and use an LCMV beamformer, because
-% this will pass the activity at the location of interest with unit gain,
-% while optimally reducing activity from other locations. This filter then
-% can be applied to the original MEG data
-
-cfg                   = [];
-cfg.covariance        = 'yes';
-cfg.vartrllength      = 2;
-cfg.covariancewindow  = 'all';
-tlock                 = ft_timelockanalysis(cfg, dataAll);
-
-cfg              = [];
-cfg.method       = 'lcmv';
-cfg.vol          = headmodel;
-cfg.grid.pos     = sourcemodel.pos(maxpowindx, :);
-cfg.grid.unit    = sourcemodel.unit;
-cfg.lcmv.keepfilter = 'yes';
-source_idx       = ft_sourceanalysis(cfg, tlock);
-
-beamformer_gam_pow = source_idx.avg.filter;
-
-gam_pow_data        = [];
-gam_pow_data.label  = {'gam_pow_x', 'gam_pow_y', 'gam_pow_z'};
-gam_pow_data.time   = dataShift.time;
-for i=1:length(dataShift.trial)
-    gam_pow_data.trial{i} = beamformer_gam_pow * tmp.trial{i};
-end
 
 %% save
 filename = sprintf('/home/electromag/matves/Results/ERF_oscillation/freq/%02d/gamma_pow_%d', subj, subj);
-save(fullfile([filename '.mat']), 'gammaPow');
+save(fullfile([filename '.mat']), 'gamPowDif');
 diary off
 movefile('tmpDiary', fullfile([filename '.txt']));
 
