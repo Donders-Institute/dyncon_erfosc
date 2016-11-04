03e6b7681858f38098f9d39645172a436c4dbb82
Mats van Es <m.vanes@donders.ru.nl>
2016-11-04 15:09:41 +0100

    analyse headmovement in a dataset

---
+++ erf_osc_headlocation.m
@@ -0,0 +1,93 @@
+function erf_osc_headlocation(subj)
+
+if nargin<1
+    subj = 4;
+end
+if isempty(subj)
+    subj = 4;
+end
+if nargin<2
+    isPilot = true;
+end
+if isempty(isPilot);
+    isPilot = true;
+end
+
+%% initiate diary
+
+workSpace = whos;
+diary('tmpDiary') % save command window output
+fname = mfilename('fullpath')
+datetime
+
+fid = fopen(fullfile([fname '.m']));
+tline = fgets(fid); % returns first line of fid
+while ischar(tline) % at the end of the script tline=-1
+    disp(tline) % display tline
+    tline = fgets(fid); % returns the next line of fid
+end
+fclose(fid);
+
+for i = 1:numel(workSpace) % list all workspace variables
+    workSpace(i).name % list the variable name
+    printstruct(eval(workSpace(i).name)) % show its value(s)
+end
+
+%% load data, define trials
+erf_osc_datainfo;
+isPilot=true;
+
+cfg=[];
+if isPilot
+    cfg.dataset = pilotsubjects(subj).dataset;
+    cfg.logfile = load(pilotsubjects(subj).logfile);% load log file
+else
+    cfg.dataset = subjects(subj).dataset;
+    cfg.logfile = load(subjects(subj).logfile);
+end
+cfg.datafile = cfg.dataset;
+cfg.headerfile = cfg.dataset;
+cfg.trialfun = 'erf_osc_mytrialfun';
+cfg.trialdef.prestim = min(cfg.logfile.log.realBaselineDuration, cfg.logfile.log.setBaselineDuration);
+cfg.trialdef.poststim = cfg.logfile.log.completeDurationGrating;
+cfg.catchtrial = cfg.logfile.log.trlNoShift;
+cfg.continuous = 'yes';
+cfg = ft_definetrial(cfg);
+
+
+%% calculate headposition
+% preprocess the headposition data
+cfg.channel                 = {'HLC0011','HLC0012','HLC0013', ...
+    'HLC0021','HLC0022','HLC0023', ...
+    'HLC0031','HLC0032','HLC0033'};
+cfg.continuous = 'yes';
+
+headpos = ft_preprocessing(cfg);
+
+% calculate the mean coil position per trial
+nTrials = length(headpos.sampleinfo);
+for iTrl = 1:nTrials
+    coil1(:,iTrl) = [mean(headpos.trial{1,iTrl}(1,:)); mean(headpos.trial{1,iTrl}(2,:)); mean(headpos.trial{1,iTrl}(3,:))];
+    coil2(:,iTrl) = [mean(headpos.trial{1,iTrl}(4,:)); mean(headpos.trial{1,iTrl}(5,:)); mean(headpos.trial{1,iTrl}(6,:))];
+    coil3(:,iTrl) = [mean(headpos.trial{1,iTrl}(7,:)); mean(headpos.trial{1,iTrl}(8,:)); mean(headpos.trial{1,iTrl}(9,:))];
+end
+
+cc = circumcenter(coil1, coil2, coil3);
+cc_mean = mean(cc,2); % mean over trials
+cc_dem = [cc - repmat(cc_mean,1,size(cc,2))]';% demean to obtain
+% translations and rotations from the average position and orientation
+cc_rel = [cc - repmat(cc(:,1),1,size(cc,2))]';% get translations and
+% rotations with reference to the first trial
+
+meanposchange = mean(abs(cc_rel(:,1:3)*1000)) % from start-end trial
+
+%% save
+if isPilot
+    filename = sprintf('/home/electromag/matves/Data/ERF_oscillation/behavioral_log/pilot/pilot_headposition_%02d', subj);
+else
+    filename = sprintf('/home/electromag/matves/Data/ERF_oscillation/behavioral_log/experiment/headposition_%02d', subj);
+end
+save(fullfile([filename '.mat']), 'cc', 'cc_dem', 'cc_rel');
+diary off
+movefile('tmpDiary', fullfile([filename, '.txt']));
+end
---
+++ plot_headlocation.m
@@ -0,0 +1,48 @@
+function plot_headlocation(subj, isPilot)
+
+if nargin<1
+    subj = 4;
+end
+if isempty(subj)
+    subj = 4;
+end
+if nargin<2
+    isPilot = true;
+end
+if isempty(isPilot);
+    isPilot = true;
+end
+
+if isPilot
+    load(sprintf('/home/electromag/matves/Data/ERF_oscillation/behavioral_log/pilot/pilot_headposition_%02d.mat', subj));
+else
+    load(sprintf('/home/electromag/matves/Data/ERF_oscillation/behavioral_log/experiment/headposition_%02d.mat', subj));
+end
+
+figure;
+subplot(1,2,1)
+hold on
+plot(cc_dem(:,1:3)*1000) % in mm
+for ii=40:40:size(cc,2)
+    h = vline(ii);
+    set(h, 'Color', [0.8 0.8 0.8], 'LineStyle', '--');
+end
+legend('x', 'y','z')
+title('mean head position per trial relative to average')
+xlabel('trial')
+ylabel('movement (mm)')
+
+% plot rotations
+subplot(1,2,2)
+hold on
+plot(cc_dem(:,4:6))
+for ii=40:40:size(cc,2)
+    h = vline(ii);
+    set(h, 'Color', [0.8 0.8 0.8], 'LineStyle', '--');
+end
+title('mean head orientation per trial relative to average')
+xlabel('trial')
+ylabel('angle (degrees)')
+legend('x', 'y','z')
+
+end
