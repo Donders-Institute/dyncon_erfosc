a04dd234839bb18664b4a42ca8cd2fefa70c6d23
Mats van Es <m.vanes@donders.ru.nl>
2017-01-27 15:06:21 +0100

    update scripts to latest naming convention and small practical improvements

--- plot_headlocation.m
+++ plot_headlocation.m
@@ -14,9 +14,9 @@ if isempty(isPilot);
 end
 
 if isPilot
-    load(sprintf('/project/3011085.02/clean/behavior/pilot-0%d/ses-01/pilot_headposition_0%d.mat', subj, subj));
+    load(sprintf('/project/3011085.02/processed/pilot-00%d/ses-beh01/pilot_headposition_0%d.mat', subj, subj));
 else
-    load(sprintf('/project/3011085.02/clean/behavior/subj-0%d/ses-01/headposition_0%d.mat', subj, subj));
+    load(sprintf('/project/3011085.02/processed/subj-00%d/ses-beh01/headposition_0%d.mat', subj, subj));
 end
 
 figure;
