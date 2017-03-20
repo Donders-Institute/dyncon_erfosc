839dae18eab664ae66a60786222276a51ea2a15c
Mats van Es <m.vanes@donders.ru.nl>
2017-03-20 09:58:03 +0100

    iteratively choose initial time window and channels with maximum amplitude

--- erf_osc_analysis_aseo.m
+++ erf_osc_analysis_aseo.m
@@ -1,4 +1,12 @@
 function erf_osc_analysis_aseo(subj, isPilot)
+% This function selects time windows for which the specific ERF peaks and
+% it selects the channels for which the amplitude is highest. Based on
+% these parameters, the ASEO algorithm (Xu et al, 2009) models the
+% event-related response and ongoing activity seperately.
+% 1. find optimal window for the peaks by selecting highest amplitude
+% channels in occipital/parietal sensors.
+% 2. find specific timewindows for every peak using globalmeanfield
+close all
 if nargin<1
     subj = 1;
 end
@@ -68,46 +76,95 @@ cfg.demean = 'yes';
 cfg.baselinewindow = [-0.05+1/fs 0];
 dataShift = ft_preprocessing(cfg, dataShift);
 
-%% Select time window
+%% baseline correction and z-scoring
 cfg=[];
 cfg.baseline = [-0.05+1/fs 0];
 cfg.baselinetype = 'abs';
 tl = ft_timelockbaseline(cfg, tlShift);
 
-t1 = nearest(tl.time, (-1+1/fs));
+t1 = nearest(tl.time, (-0.5+1/fs));
 t2 = nearest(tl.time, 0);
 s = std(tl.avg(:,t1:t2), [], 2);
 m = mean(tl.avg(:, t1:t2), 2);
 M = repmat(m, [1, size(tl.avg,2)]);
 
 % two z scoring options: x/s or (x-m)/s?
-y=tl;
-y.avg=diag(1./s)*(tl.avg-M);
-z = ft_globalmeanfield([], y);
+tlZscore=tl;
+tlZscore.avg=diag(1./s)*(tl.avg-M);
+gmf = ft_globalmeanfield([], tlZscore);
 
-y2=tl;
-y2.avg = diag(1./s)*(tl.avg);
-z2 = ft_globalmeanfield([], y2);
+% y2=tl;
+% y2.avg = diag(1./s)*(tl.avg);
+% z2 = ft_globalmeanfield([], y2);
 
-figure; subplot(2,1,1); plot(z.time, z.avg); title('(X-M)/s'); xlim([-0.05 0.5]);
-subplot(2,1,2); cfg=[]; cfg.xlim = [-0.05 0.5]; ft_singleplotER(cfg, tl);
-window = input('What is the latency window of every individual peak?');
-% window = [0.056 0.094; 0.094 0.1517; 0.155 0.2117];
-for i=1:size(window, 1)
-    tmp = window(i,2)-window(i,1);
-    tmp = tmp/4;
-    windowvar(i,:) = [-tmp tmp];
-end
 
-%% Select channels
-% repeat this for all peaks
-for iPeak = 1:size(window, 1)
-    
-    % select critical window 0:300 ms
+%% Select time window
+% take a first guess at the time window, select channels with highest
+% amplitudes in this window. Then base more precise estimation of time
+% window these channels
+
+guess = [0.050 0.090; 0.095 0.130; 0.135 0.180];
     cfg=[];
-    cfg.latency = [window(iPeak,1) window(iPeak,2)];
+    cfg.layout = 'CTF275_helmet.mat';
+    tmp = rmfield(tlZscore, 'cfg');
+    ft_topoplotER(cfg, tlZscore);
+    for iPeak = 1:size(guess,1)
+        windowGuess{iPeak} = input('What is the initial estimate of the peak latencies?')
+    end
+for iPeak=1:size(guess,1)
+    
+    cfg=[];
+    cfg.latency = [windowGuess{iPeak}(iPeak,1) windowGuess{iPeak}(iPeak,2)];
     cfg.channel = {'MRP', 'MLP', 'MZP', 'MZO', 'MLO', 'MRO'};
-    critWin = ft_selectdata(cfg, tlShift);
+    critWin_guess = ft_selectdata(cfg, tlZscore);
+    
+    % take mean of every channel
+    avgChan_guess = mean(critWin_guess.avg, 2);
+    
+    % find 8 channels with max abs amplitude in critical window, then seperate
+    % them into negative and positive (pole). Average them accordingly after
+    % 'flipping' the negative pole
+    selChan_1 = 8;
+    [~, maxAbsIdx_guess] = sort(abs(avgChan_guess), 1, 'descend');
+    maxAbsIdx_guess = maxAbsIdx_guess(1:selChan_1);
+    chanIdxPos_guess = maxAbsIdx_guess(find(avgChan_guess(maxAbsIdx_guess)>0));
+    chanIdxNeg_guess = maxAbsIdx_guess(find(avgChan_guess(maxAbsIdx_guess)<0));
+    
+    cfg=[];
+    cfg.channel = critWin_guess.label(chanIdxPos_guess);
+    dataPos_guess = ft_selectdata(cfg, tlZscore);
+    cfg.channel = critWin_guess.label(chanIdxNeg_guess);
+    dataNeg_guess = ft_selectdata(cfg, tlZscore);
+    %
+    cfg = [];
+    cfg.operation = 'multiply';
+    cfg.scalar = -1;
+    cfg.parameter = 'avg';
+    dataNegFlip_guess = ft_math(cfg, dataNeg_guess);
+    
+    erfdata_selchan = ft_appenddata([], dataPos_guess, dataNegFlip_guess);
+    
+    gmf_selchan = ft_globalmeanfield([], erfdata_selchan);
+    
+    % plot globalmeanfield and z-scores timelock data
+    figure; cfgp=[]; cfgp.xlim = [windowGuess{iPeak}(iPeak,1) windowGuess{iPeak}(iPeak,2)]; cfgp.layout = 'CTF275_helmet.mat'; ft_topoplotER(cfgp, tlZscore)
+    figure; subplot(2,1,1); plot(gmf_selchan.time, gmf.avg);hold on; plot(gmf_selchan.time, gmf_selchan.avg); title('(X-M)/s'); xlim([-0.05 0.5]);
+    subplot(2,1,2); cfg=[]; cfg.xlim = [-0.05 0.5]; ft_singleplotER(cfg, erfdata_selchan); hold on; hline(0);
+    window{iPeak} = input('What is the latency window of every individual peak?');
+    
+    for i=1:size(window{iPeak}, 1)
+        lat = mean([window{iPeak}(i,2) window{iPeak}(i,1)]);
+        var = max(lat/5, 0.010); % lowest peak jitter is 10 ms
+        windowvar{iPeak}(i,:) = [-var var];
+    end
+    
+    %% Select channels
+    
+    % select critical window
+    cfg=[];
+    cfg.latency = [window{iPeak}(iPeak,1) window{iPeak}(iPeak,2)];
+    cfg.channel = {'MRP', 'MLP', 'MZP', 'MZO', 'MLO', 'MRO'};
+    critWin = ft_selectdata(cfg, tlZscore);
     
     % take mean of every channel
     avgChan = mean(critWin.avg, 2);
@@ -129,11 +186,12 @@ for iPeak = 1:size(window, 1)
     dataNeg = ft_selectdata(cfg, dataShift);
     %
     cfg = [];
-    cfg.operation = '-x1';
+    cfg.operation = 'multiply';
+    cfg.scalar = -1;
     cfg.parameter = 'trial';
-    tmp = ft_math(cfg, dataNeg);
+    dataNegFlip = ft_math(cfg, dataNeg);
     
-    erfdata = ft_appenddata([], dataPos, tmp);
+    erfdata = ft_appenddata([], dataPos, dataNegFlip);
     cfg=[];
     cfg.avgoverchan = 'yes';
     erfdata = ft_selectdata(cfg, erfdata);
@@ -148,11 +206,13 @@ for iPeak = 1:size(window, 1)
     
     cfg                      = [];
     cfg.method               = 'aseo';
-    cfg.aseo.waveformInitSet = window
+    cfg.aseo.waveformInitSet = window{iPeak};
     cfg.aseo.numiteration    = 1;
-    cfg.aseo.jitter          = windowvar
+    cfg.aseo.jitter          = windowvar{iPeak};
     [reconstructed{iPeak}, residual{iPeak}] = ft_singletrialanalysis(cfg, erfdata);
 end
+% Combine the latency and amplitude parameters from the ASEO applied to the
+% specific peaks (with specific timewindow and channel parameters)
 for iPeak = 1:size(window,1)
     latency(:, iPeak) = reconstructed{iPeak}.params.latency(:, iPeak);
     amplitude(:, iPeak) = reconstructed{iPeak}.params.amplitude(:, iPeak);
@@ -164,7 +224,7 @@ if isPilot
 else
     filename = sprintf('/project/3011085.02/results/erf/sub-%03d/aseo', subj);
 end
-save(fullfile([filename '.mat']), 'erfdata', 'reconstructed', 'residual', 'latency', 'amplitude')
+save(fullfile([filename '.mat']), 'erfdata', 'reconstructed', 'residual', 'latency', 'amplitude', 'window', 'windowGuess', 'windowvar')
 diary off
 movefile(diaryname, fullfile([filename, '.txt']));
 
