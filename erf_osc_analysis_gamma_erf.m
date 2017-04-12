92c3113f6deeabdf40c22d2278730292f6eb0e55
Mats van Es <m.vanes@donders.ru.nl>
2017-04-12 09:54:20 +0200

    bin trials (raw data) based on maximum gamma power. Regression of gamma power on erf over time

--- erf_osc_analysis_gamma_erf.m
+++ erf_osc_analysis_gamma_erf.m
@@ -12,6 +12,7 @@ end
 if isempty(isPilot);
     isPilot = false;
 end
+close all
 
 %% Initiate Diary
 workSpace = whos;
@@ -42,6 +43,7 @@ if isPilot
 else
     data = load(sprintf('/project/3011085.02/processed/sub-%03d/ses-meg01/cleandata.mat', subj), 'dataClean');
     load(sprintf('/project/3011085.02/results/freq/sub-%03d/gamma_virtual_channel.mat', subj), 'gammaChan');
+    load(sprintf('/project/3011085.02/results/erf/sub-%03d/timelock.mat', subj));
 end
 
 data = data.dataClean;
@@ -67,32 +69,76 @@ cfg=[];
 cfg.baseline = [-0.050+1/fs 0];
 data = ft_timelockbaseline(cfg, dataShift);
 
+cfg=[];
+cfg.latency = [-4.95 0.7]; % this will ensure the betas have the same size for all subjects
+data = ft_selectdata(cfg, data);
 
 for i=1:length(gammaChan.trial)
-    gammaPow(i) = gammaChan.trial(i).pow;
+    gammaPow(i) = log(gammaChan.trial(i).pow);
 end
 
+
+%% mutual information / GLM
+% rt = (data.trialinfo(:,6)-data.trialinfo(:,5))'; 
+design = [ones(size(gammaPow)); gammaPow-mean(gammaPow)];
+cfgp=[];
+cfgp.layout = 'CTF275_helmet.mat';
+cfgp.latency = [0 0.5];
+ft_topoplotER(cfgp, tlShift);
+
+% channel = ;%input('what is the maximum channel?')
+% load(sprintf('/project/3011085.02/results/erf/sub-%03d/erf_gamma.mat', subj), 'channel')
+
+% ch_idx = find(strcmp(channel, data.label));
+for k=1:length(data.label)
+Y = squeeze(data.trial(:,k,:));
+betas(:,:,k) = design'\Y;
+end
+
+
 %% Sort and bin trials on gamma power
-groupSize = round(nTrials/4);
+groupSize = round(nTrials/5);
 [~, idxMax] = sort(gammaPow, 2, 'descend');
-idxMax = idxMax(1:groupSize);
-[~, idxMin] = sort(gammaPow, 2, 'ascend');
-idxMin = idxMin(1:groupSize);
+% idxMax = idxMax(1:groupSize);
+% [~, idxMin] = sort(gammaPow, 2, 'ascend');
+% idxMin = idxMin(1:groupSize);
+data.trial = data.trial(idxMax,:,:);
+% data.dimord = 'rpt_chan_time';
+meangamma = mean(gammaPow);
+% cfg=[];
+% cfg.trials = idxMax;
+% cfg.avgoverrpt = 'yes';
+% cfg.latency = [-0.1 inf];
+% maxGam = ft_selectdata(cfg, data);
+% cfg.trials = idxMin;
+% minGam = ft_selectdata(cfg, data);
+cfgb=[];
+cfgb.vartrllength = 2;
+init = 1;
+for i = 1:round((nTrials/groupSize)*2)-1
+    if init+groupSize-1>nTrials
+        z = nTrials;
+    else
+        z = init+groupSize-1;
+    end
+   cfg=[];
+   cfg.trials = idxMax(init:z);
+   tmp = ft_selectdata(cfg, data);
+   erf{i} = ft_timelockanalysis(cfgb, tmp);
+   cfg=[];
+   cfg.baseline = [-0.05+1/fs 0];
+   erf{i} = ft_timelockbaseline(cfg, erf{i});
+   erf{i}.gamma = mean(gammaPow(idxMax(init:z))-meangamma);
+   init=init+round(groupSize/2);
+end
 
-cfg=[];
-cfg.trials = idxMax;
-maxGam = ft_selectdata(cfg, data);
-cfg.trials = idxMin;
-minGam = ft_selectdata(cfg, data);
-
-
-%% Timelock analysis
-cfg=[];
-cfg.vartrllength = 2;
-tlMax = ft_timelockanalysis(cfg, maxGam);
-tlMin = ft_timelockanalysis(cfg, minGam);
-
-
+erfdata=data;
+erfdata.trial=[];
+for i=1:length(erf)
+    erfdata.trial{i} = erf{i}.avg;
+    erfdata.var{i} = erf{i}.var;
+    gamma(1,i) = erf{i}.gamma;
+end
 
 
 %% Save
@@ -101,7 +147,7 @@ if isPilot
 else
     filename = sprintf('/project/3011085.02/results/erf/sub-%03d/erf_gamma', subj);
 end
-save(fullfile([filename '.mat']), 'tlMax', 'tlMin');
+save(fullfile([filename '.mat']), 'betas', 'erfdata', 'gamma');
 diary off
 movefile(diaryname, fullfile([filename '.txt']));
 
