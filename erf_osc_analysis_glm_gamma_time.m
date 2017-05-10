2b98b9411258df9b7cbd1d631fcd5a88bb50cb07
Mats van Es <m.vanes@donders.ru.nl>
2017-05-10 14:09:42 +0200

    don't do regression on gamma binned trials, but on single trials. delete redundant/unnecessary parameters

--- erf_osc_analysis_glm_gamma_time.m
+++ erf_osc_analysis_glm_gamma_time.m
@@ -1,4 +1,4 @@
-function erf_osc_analysis_glm_gamma_time(subj, isPilot, zscore)
+function erf_osc_analysis_glm_gamma_time(subj, isPilot)
 % do a linear regression of pre-change gamma power over time.
 if nargin<1
     subj = 1;
@@ -11,12 +11,6 @@ if nargin<2
 end
 if isempty(isPilot);
     isPilot = false;
-end
-if nargin<3
-    zscore = false;
-end
-if isempty(zscore);
-    zscore = false;
 end
 
 % close all
@@ -55,72 +49,12 @@ end
 fs=data_dss.fsample;
 for i=1:length(gammaChan.trial)
     gammaPow(i) = log(gammaChan.trial(i).pow);
-    gammaPow2(i) = (gammaChan.trial(i).pow);
+%     gammaPow2(i) = (gammaChan.trial(i).pow);
 end
 gammaPow = gammaPow-mean(gammaPow);
 nTrials = length(data_dss.trial);
 
 [~, idxMax] = sort(gammaPow, 2, 'descend');
-
-%% GLM on binned trials
-% groupSize = round(nTrials/5);
-% 
-% erfdata = data_dss;
-% cfg=[];
-% cfg.lpifilter = 'yes';
-% cfg.lpfilttype = 'firws';
-% erfdata = ft_preprocessing(cfg, erfdata);
-% erfdata.time=erfdata.time{1};
-% erfdata.trial=erfdata.trial(idxMax);
-% erfdata.trialinfo=erfdata.trialinfo(idxMax,:);
-% erfdata.trial = cat(3, erfdata.trial{:});
-% erfdata.trial = permute(erfdata.trial, [3,1,2]);
-% 
-% 
-% cfgb=[];
-% cfgb.vartrllength = 2;
-% init = 1;
-% for i = 1:round((nTrials/groupSize)*2)-1
-%     if init+groupSize-1>nTrials
-%         z = nTrials;
-%     else
-%         z = init+groupSize-1;
-%     end
-%     cfg=[];
-%     cfg.trials = init:z;
-%     tmp = ft_selectdata(cfg, erfdata);
-%     erf{i} = ft_timelockanalysis(cfgb, tmp);
-%     var = erf{i}.var;
-%     cfg=[];
-%     cfg.baseline = [-0.05+1/fs 0];
-%     erf{i} = ft_timelockbaseline(cfg, erf{i});
-%     gamma(i) = mean(gammaPow2(idxMax(init:z)));
-%     erf{i}.var = var;
-%     init=init+round(groupSize/2);
-% end
-% gamma = log(gamma);
-% gamma = gamma-mean(gamma);
-% 
-% erfdata.trial = [];
-% for i = 1:round((nTrials/groupSize)*2)-1
-%     erfdata.trial(i,:,:) = erf{i}.avg;
-%     erfdata.var(i,:,:) = erf{i}.var;
-% end
-% 
-% if zscore
-%     t1 = nearest(erfdata.time, (-0.5+1/fs));
-%     t2 = nearest(erfdata.time, 0);
-%     S = sqrt(erfdata.var);
-%     M = mean(erfdata.trial(:,:,t1:t2),3);
-%     M = repmat(M,[1,1,length(erfdata.time)]);
-%     erfdata.trial=(erfdata.trial-M)./S;
-% end
-% 
-% design = [ones(size(gamma)); gamma];
-% for k=1:length(erfdata.label)
-%     Y = squeeze(erfdata.trial(:,k,:));
-%     betas_bin(:,:,k) = design'\Y;
-% end
 
 %% GLM on all trials
 design = [ones(size(gammaPow)); gammaPow];
@@ -128,6 +62,7 @@ data=data_dss;
 cfg=[];
 cfg.lpifilter = 'yes';
 cfg.lpfilttype = 'firws';
+cfg.lpfreq = 40;
 data = ft_preprocessing(cfg, data);
 data.trial = cat(3,data.trial{:});
 data.trial = permute(data.trial, [3,1,2]);
@@ -147,9 +82,26 @@ for k=1:length(data.label)
     Y = squeeze(data.trial(:,k,:));
     betas_trials(:,:,k) = design'\Y;
 end
-c =[191   203   192   203   186   195];
-t1 = find(data.time>0,1);
-Y_trials = squeeze(data.trial(:,:,t1:t1+419));
+
+
+%% planar gradiant transformation of beta weights
+% put beta weights in timelock structure
+tl=[];
+tl.avg    = squeeze(betas_trials(2,:,:))';
+tl.time   = data_dss.time{1};
+tl.dimord = 'chan_time';
+tl.label  = data_dss.label;
+tl.grad   = data_dss.grad;
+
+% planar combination
+cfg                 = [];
+cfg.feedback        = 'no';
+cfg.method          = 'template';
+cfg.neighbours      = ft_prepare_neighbours(cfg, tl);
+cfg.planarmethod    = 'sincos';
+tlPlanar            = ft_megplanar(cfg, tl);
+cfg                 = [];
+tlPlanarCmb         = ft_combineplanar(cfg,tlPlanar);
 
 
 %% Save
@@ -158,7 +110,7 @@ if isPilot
 else
     filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_gamma_time', subj);
 end
-save(fullfile([filename '.mat']), 'betas_trials', 'erfdata','Y_trials', '-v7.3');
+save(fullfile([filename '.mat']), 'betas_trials', 'tlPlanarCmb', '-v7.3');
 diary off
 movefile(diaryname, fullfile([filename '.txt']));
 
