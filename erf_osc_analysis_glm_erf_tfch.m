d1a903b86cd68833d29893fda0979f255cd560d5
Mats van Es <m.vanes@donders.ru.nl>
2017-05-12 17:56:22 +0200

    do regression of p1 amplitude on time, frequency and channel. Estimate p1 amplitude from window where max amplitude channel is highest.

--- erf_osc_analysis_glm_erf_tfch.m
+++ erf_osc_analysis_glm_erf_tfch.m
@@ -1,5 +1,5 @@
-function erf_osc_analysis_glm_erf_tfch(subj, isPilot, glm)
-% linear regression of peak amplitude over time-frequency (with fixed 
+function erf_osc_analysis_glm_erf_tfch(subj, isPilot)
+% linear regression of peak amplitude over time-frequency (with fixed
 % channel) or over frequency-channel (with fixed (avg) time).
 
 if nargin<1
@@ -14,12 +14,7 @@ end
 if isempty(isPilot);
     isPilot = false;
 end
-if nargin<3
-    glm = 'fixchan'; % other option is 'fixtime'
-end
-if isempty(glm)
-    glm='fixchan';
-end
+
 
 
 %% Initiate Diary
@@ -56,114 +51,93 @@ end
 fs=data_dss.fsample;
 nTrials = length(data_dss.trial);
 
-%%
-% first six subjects, maximum channels
-c(1)=match_str(data_dss.label,'MRO22');
-c(2)=match_str(data_dss.label,'MRO52');
-c(3)=match_str(data_dss.label,'MRO23');
-c(4)=match_str(data_dss.label,'MRO52');
-c(5)=match_str(data_dss.label,'MRO11');
-c(6)=match_str(data_dss.label,'MRO32');
-lat = [0.09917, 0.06333, 0.1092, 0.07417, 0.07, 0.11];
+%% select p1 window for regression
 
-if strcmp(glm, 'fixtime')
-    %% all channels, one time
+tlck = ft_timelockanalysis([], data_dss);
+t1 = nearest(tlck.time, 0.06);
+t2 = nearest(tlck.time, 0.12);
+cfg=[];
+cfg.latency = [tlck.time(t1) tlck.time(t2)];
+tlck = ft_selectdata(cfg, tlck);
+
+
+time = tlck.time;
+[~, maxchan] = max(abs(mean(tlck.avg,2))); % find channel with max amplitude
+% calculate mean over every window to find out which window has the maximum
+% mean amplitude (for the maximum channel!). Take the mean amplitude in 
+% this latency window as regression weight.
+halfwindowlength = 8;
+i=1;
+for t = halfwindowlength+1:length(time)-halfwindowlength;
+    win(i,:) = [time(t-8), time(t+8)];
+    avg(i,1) = mean(tlck.avg(maxchan,t-8:t+8),2);
+    i=i+1;
+end
+[~, window] = max(abs(avg));
+lat = win(window,:);
+
+% first six subjects, maximum channels
+% c(1)=match_str(data_dss.label,'MRO22');
+% c(2)=match_str(data_dss.label,'MRO52');
+% c(3)=match_str(data_dss.label,'MRO23');
+% c(4)=match_str(data_dss.label,'MRO52');
+% c(5)=match_str(data_dss.label,'MRO11');
+% c(6)=match_str(data_dss.label,'MRO32');
+% lat = [0.09917, 0.06333, 0.1092, 0.07417, 0.07, 0.11];
+
+ %% Regression p1 amplitude over time-frequency-channel
+    %select data only for maximum channel
     cfg=[];
-    cfg.parameter = 'powspctrm';
-    cfg.baselinetype = 'absolute';
-    cfg.baseline = [-0.6 -0.1];
-    h = ft_freqbaseline(cfg, tfaHigh);
-    l = ft_freqbaseline(cfg, tfaLow);
-    
-    cfg=[];
-    cfg.latency=[0.05 0.15];
-    cfg.avgovertime='yes';
-    l = ft_selectdata(cfg, l);
-    h = ft_selectdata(cfg, h);
-    
-    cfg=[];
-    cfg.channel = data_dss.label(c(subj));
-    d  =ft_selectdata(cfg, data_dss);
-    d.trial = cat(1,d.trial{:});
-    
-    idx = nearest(d.time{1}, lat(subj));
-    p1 = d.trial(:,idx)'; % p1 amplitude per trial
-    
-    design = [ones(size(p1)); p1];
-    
-    for freq=1:19
-        Y_h = (squeeze(h.powspctrm(:,:,freq)));
-        betas_h(:,:,freq) = design'\Y_h;
-    end
-    bh = h;
-    bh.powspctrm = betas_h(2,:,:); % save betas in powspctrm structure, in right format for later use of ft_freqdescriptives.
-    bh.dimord='rpt_chan_freq';
-    
-    % do the same for low frequency TFR
-    for freq=1:15
-        Y_l = squeeze(l.powspctrm(:,:,freq));
-        betas_l(:,:,freq) = design'\Y_l;
-    end
-    bl = l;
-    bl.powspctrm = betas_l(2,:,:);
-    bl.dimord='rpt_chan_freq';
-    
-    
-elseif strcmp(glm, 'fixchan')
-    %% one channel, over time
-    select data only for maximum channel
-    cfg=[];
-    cfg.channel = data_dss.label(c(subj));
+    cfg.channel = 'all';%data_dss.label(c(subj));
     l = ft_selectdata(cfg, tfaLow);
     h = ft_selectdata(cfg, tfaHigh);
     d  =ft_selectdata(cfg, data_dss);
-    d.trial = cat(1,d.trial{:});
+    d.trial = cat(3,d.trial{:});
     
-    idx = nearest(d.time{1}, lat(subj));
-    p1 = d.trial(:,idx)'; % p1 amplitude per trial
-    
-    design = [ones(size(p1)); p1];
-    
-    baseline correct TFR
+    % baseline correct TFR
     cfg=[];
     cfg.parameter = 'powspctrm';
-    cfg.baselinetype = 'absolute';
+    cfg.baselinetype = 'relchange';
     cfg.baseline = [-0.6 -0.1];
     h = ft_freqbaseline(cfg, h);
     l = ft_freqbaseline(cfg, l);
     
+    idx = [nearest(d.time{1}, lat(1)), nearest(d.time{1}, lat(2))];
+    p1 = squeeze(mean(d.trial(:,idx,:),2));
+    
     for freq=1:19
-        Y_h = squeeze(squeeze(h.powspctrm(:,:,freq,:)));
-        betas_h(:,1,:,freq) = design'\Y_h;
+        for ch=1:length(d.label);
+            design = p1(ch,:);
+            Y_h = squeeze(squeeze(h.powspctrm(:,ch,freq,:)));
+            betas_h(freq,ch,:) = design'\Y_h;
+        end
     end
-    bh = h;
-    bh.powspctrm = permute(betas_h(2,:,:,:), [1,2,4,3]); % save betas in powspctrm structure, in right format for later use of ft_freqdescriptives.
-    bh.label = {'chan01'};
+    bh=h;
+    bh.powspctrm = permute(betas_h, [2,1,3]);
+    bh.label=h.label;
+    bh.dimord = 'chan_freq_time';
     
     % do the same for low frequency TFR
     for freq=1:15
-        Y_l = squeeze(squeeze(l.powspctrm(:,:,freq,:)));
-        betas_l(:,1,:,freq) = design'\Y_l;
+        for ch=1:length(d.label);
+            design = p1(ch,:);
+            Y_l = squeeze(squeeze(l.powspctrm(:,ch,freq,:)));
+            betas_l(freq,ch,:) = design'\Y_l;
+        end
+        bl = l;
+        bl.powspctrm = permute(betas_l, [2,1,3]);
+        bh.label=h.label;
+        bh.dimord = 'chan_freq_time';
     end
-    bl = l;
-    bl.powspctrm = permute(betas_l(2,:,:,:), [1,2,4,3]);
-    bl.label={'chan01'};
+    
     
     %% Save
     if isPilot
-        if strcmp(glm, 'fixtime')
-            filename = sprintf('/project/3011085.02/results/erf/pilot-%03d/glm_erf_fch', subj);
-        elseif strcmp(glm, 'fixchan')
             filename = sprintf('/project/3011085.02/results/erf/pilot-%03d/glm_erf_tf', subj);
-        end
     else
-        if strcmp(glm, 'fixtime')
-            filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_erf_fch', subj);
-        elseif strcmp(glm, 'fixch')
             filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_erf_tf', subj);
-        end
     end
-    save(fullfile([filename '.mat']),'betas_h', 'betas_l', 'bl', 'bh','-v7.3');
+    save(fullfile([filename '.mat']),'betas_h', 'betas_l', 'bl', 'bh', 'betas_h', '-v7.3');
     diary off
     movefile(diaryname, fullfile([filename '.txt']));
     
