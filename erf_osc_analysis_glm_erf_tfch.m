788fd0922a3563d2250ffe69f317ef2e537d62bf
Mats van Es <m.vanes@donders.ru.nl>
2017-05-18 17:03:41 +0200

    change baseline correction to average baseline instead of single-trial. Add option for either doing regression on high, or on low frequencies. Also add option for using TF-data timelocked to stimulus onset or stimulus reversal

--- erf_osc_analysis_glm_erf_tfch.m
+++ erf_osc_analysis_glm_erf_tfch.m
@@ -1,4 +1,4 @@
-function erf_osc_analysis_glm_erf_tfch(subj, isPilot)
+function erf_osc_analysis_glm_erf_tfch(subj, isPilot, freqRange, zeropoint;)
 % linear regression of peak amplitude over time-frequency (with fixed
 % channel) or over frequency-channel (with fixed (avg) time).
 
@@ -14,8 +14,18 @@ end
 if isempty(isPilot);
     isPilot = false;
 end
-
-
+if nargin<3
+    freqRange = 'high'; % can be 'high' or 'low'; depending on which frequency range you want to do regression (2-30 Hz or 28-100 Hz)
+end
+if isempty(freqRange)
+    freqRange = 'high';
+end
+if nargin<4
+    zeropoint = 'onset'; % other option 'reversal': redefine time axis to stimulus reversal or keep it at stimulus onset
+end
+if isempty(zeropoint);
+    zeropoint = 'onset';
+end
 
 %% Initiate Diary
 workSpace = whos;
@@ -44,7 +54,7 @@ if isPilot
     load(sprintf('/project/3011085.02/results/erf/pilot-%03d/dss.mat', subj), 'data_dss');
     load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_virtual_channel.mat', subj), 'gammaChan');
 else
-    load(sprintf('/project/3011085.02/results/freq/sub-%03d/tfa.mat', subj));
+    load(sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%d.mat', subj, zeropoint));
     load(sprintf('/project/3011085.02/results/erf/sub-%03d/dss.mat', subj), 'data_dss');
     %     load(sprintf('/project/3011085.02/results/erf/sub-%03d/timelock.mat', subj));
 end
@@ -54,10 +64,10 @@ nTrials = length(data_dss.trial);
 %% select p1 window for regression
 
 tlck = ft_timelockanalysis([], data_dss);
-t1 = nearest(tlck.time, 0.06);
-t2 = nearest(tlck.time, 0.12);
+t1p1 = nearest(tlck.time, 0.06);
+t2p1 = nearest(tlck.time, 0.12);
 cfg=[];
-cfg.latency = [tlck.time(t1) tlck.time(t2)];
+cfg.latency = [tlck.time(t1p1) tlck.time(t2p1)];
 tlck = ft_selectdata(cfg, tlck);
 
 
@@ -76,74 +86,86 @@ end
 [~, window] = max(abs(avg));
 lat = win(window,:);
 
-% first six subjects, maximum channels
-% c(1)=match_str(data_dss.label,'MRO22');
-% c(2)=match_str(data_dss.label,'MRO52');
-% c(3)=match_str(data_dss.label,'MRO23');
-% c(4)=match_str(data_dss.label,'MRO52');
-% c(5)=match_str(data_dss.label,'MRO11');
-% c(6)=match_str(data_dss.label,'MRO32');
-% lat = [0.09917, 0.06333, 0.1092, 0.07417, 0.07, 0.11];
-
 %% Regression p1 amplitude over time-frequency-channel
-%select data only for maximum channel
-cfg=[];
-cfg.channel = 'all';%data_dss.label(c(subj));
-l = ft_selectdata(cfg, tfaLow);
-h = ft_selectdata(cfg, tfaHigh);
-d  =ft_selectdata(cfg, data_dss);
-d.trial = cat(3,d.trial{:});
 
-% baseline correct TFR
-cfg=[];
-cfg.parameter = 'powspctrm';
-cfg.baselinetype = 'relchange';
-cfg.baseline = [-0.6 -0.1];
-h = ft_freqbaseline(cfg, h);
-l = ft_freqbaseline(cfg, l);
+trialdata = cat(3,data_dss.trial{:});
 
-idx = [nearest(d.time{1}, lat(1)), nearest(d.time{1}, lat(2))];
-p1 = squeeze(mean(d.trial(:,idx,:),2));
+idxtime = [nearest(data_dss.time{1}, lat(1)) : nearest(data_dss.time{1}, lat(2))];
+p1amp = squeeze(mean(trialdata(:,idxtime,:),2));
 
-for freq=1:19
-    for ch=1:length(d.label);
-        design = p1(ch,:);
-        Y_h = squeeze(squeeze(h.powspctrm(:,ch,freq,:)));
-        betas_h(freq,ch,:) = design'\Y_h;
+% baselinecorrect with average baseline over trials
+if strcmp(freqRange, 'high');
+    cfg=[];
+    cfg.latency = [-1+1/fs -0.25];
+    cfg.avgoverrpt = 'yes';
+    cfg.avgovertime = 'yes';
+    baselineH = ft_selectdata(cfg, tfaHigh);
+    baselineH.time = tfaHigh.time;
+    baselineH.dimord = tfaHigh.dimord;
+    baselineH.powspctrm = repmat(baselineH.powspctrm, [1,1,length(baselineH.time), size(tfaHigh.powspctrm, 1)]);
+    baselineH.powspctrm = permute(baselineH.powspctrm, [4,1,2,3]);
+    cfg = [];
+    cfg.parameter = 'powspctrm';
+    cfg.operation = 'subtract';
+    tfaHigh = ft_math(cfg, tfaHigh, baselineH);
+    
+    for freq=1:19
+        for ch=1:length(d.label);
+            design = [ones(size(p1amp(ch,:))); p1amp(ch,:)];
+            design(2,:) = (design(2,:)-mean(design(2,:)))./std(design(2,:));
+            Y_h = squeeze(squeeze(tfaHigh.powspctrm(:,ch,freq,:)));
+            betas_h(freq,ch,:,:) = design'\Y_h;
+        end
+    end
+    
+elseif strcmp(freqRange, 'low')
+    % do the same for low frequency TFR
+    cfg=[];
+    cfg.latency = [-1+1/fs -0.25];
+    cfg.avgoverrpt = 'yes';
+    cfg.avgovertime = 'yes';
+    baselineL = ft_selectdata(cfg, tfaLow);
+    baselineL.time = tfaLow.time;
+    baselineL.dimord = tfaLow.dimord;
+    baselineL.powspctrm = repmat(baselineL.powspctrm, [1,1,length(baselineL.time), size(tfaLow.powspctrm, 1)]);
+    baselineL.powspctrm = permute(baselineL.powspctrm, [4,1,2,3]);
+    cfg = [];
+    cfg.parameter = 'powspctrm';
+    cfg.operation = 'subtract';
+    tfaLow = ft_math(cfg, tfaLow, baselineL);
+    
+    for freq=1:15
+        for ch=1:length(d.label);
+            design = [ones(size(p1amp(ch,:))); p1amp(ch,:)];
+            design(2,:) = (design(2,:)-mean(design(2,:)))./std(design(2,:));
+            Y_l = squeeze(squeeze(tfaLow.powspctrm(:,ch,freq,:)));
+            betas_l(freq,ch,:,:) = design'\Y_l;
+        end
     end
 end
-
-% do the same for low frequency TFR
-for freq=1:15
-    for ch=1:length(d.label);
-        design = p1(ch,:);
-        Y_l = squeeze(squeeze(l.powspctrm(:,ch,freq,:)));
-        betas_l(freq,ch,:) = design'\Y_l;
-    end
-end
-
 
 %% planar gradiant transformation of beta weights
 % make timelocked structure where planar gradient transformation can be
 % applied to (that's why dimord is strange)
-tlh=[];
-tlh.avg    = betas_h;
-tlh.time   = h.time;
-tlh.dimord = 'subj_chan_time';
-tlh.label  = h.label;
-tlh.grad   = h.grad;
-%
-% % planar combination
-cfg                 = [];
-cfg.feedback        = 'no';
-cfg.method          = 'template';
-cfg.neighbours      = ft_prepare_neighbours(cfg, tlh);
-cfg.planarmethod    = 'sincos';
-tlhPlanar           = ft_megplanar(cfg, tlh);
-
-% demeaning/baseline correction not possible on this data structure. Do
-% manual baseline correction. NOT NECESSARY. DOESN'T CHANGE ANYTHING
-%{
+if strcmp(freqRange, 'high')
+    tlh=[];
+    tlh.avg    = squeeze(betas_h(:,:,2,:));
+    tlh.time   = tfaHigh.time;
+    tlh.dimord = 'subj_chan_time';
+    tlh.label  = tfaHigh.label;
+    tlh.grad   = tfaHigh.grad;
+    %
+    % % planar combination
+    cfg                 = [];
+    cfg.feedback        = 'no';
+    cfg.method          = 'template';
+    cfg.neighbours      = ft_prepare_neighbours(cfg, tlh);
+    cfg.planarmethod    = 'sincos';
+    tlhPlanar           = ft_megplanar(cfg, tlh);
+    
+    % demeaning/baseline correction not possible on this data structure. Do
+    % manual baseline correction. NOT NECESSARY. DOESN'T CHANGE ANYTHING
+    %{
 t3            = nearest(tlhPlanar.time, -0.6);
 t4            = nearest(tlhPlanar.time, -0.1);
 blcorr        = rmfield(tlhPlanar, 'trial');
@@ -152,79 +174,92 @@ cfg           = [];
 cfg.operation = 'subtract';
 cfg.parameter = 'trial';
 tlhPlanar = ft_math(cfg, tlhPlanar, blcorr);
-%}
-cfg           = [];
-bhPlanarCmb  = ft_combineplanar(cfg,tlhPlanar);
-
-% put it back in a freq-data structure
-bhPlanarCmb.powspctrm = permute(bhPlanarCmb.trial, [2,1,3]);
-bhPlanarCmb           = rmfield(bhPlanarCmb, 'trial');
-bhPlanarCmb.freq      = h.freq;
-bhPlanarCmb.dimord    = 'chan_freq_time';
-
-% Do the same for low frequencies.
-tll=[];
-tll.avg    = betas_l;
-tll.time   = l.time;
-tll.dimord = 'subj_chan_time';
-tll.label  = l.label;
-tll.grad   = l.grad;
-%
-% % planar combination
-cfg                 = [];
-cfg.feedback        = 'no';
-cfg.method          = 'template';
-cfg.neighbours      = ft_prepare_neighbours(cfg, tll);
-cfg.planarmethod    = 'sincos';
-tllPlanar           = ft_megplanar(cfg, tll);
-
-cfg           = [];
-blPlanarCmb  = ft_combineplanar(cfg,tllPlanar);
-
-% put it back in a freq-data structure
-blPlanarCmb.powspctrm = permute(blPlanarCmb.trial, [2,1,3]);
-blPlanarCmb           = rmfield(blPlanarCmb, 'trial');
-blPlanarCmb.freq      = l.freq;
-blPlanarCmb.dimord    = 'chan_freq_time';
-
+    %}
+    cfg           = [];
+    bhPlanarCmb  = ft_combineplanar(cfg,tlhPlanar);
+    
+    % put it back in a freq-data structure
+    bhPlanarCmb.powspctrm = permute(bhPlanarCmb.trial, [2,1,3]);
+    bhPlanarCmb           = rmfield(bhPlanarCmb, 'trial');
+    bhPlanarCmb.freq      = tfaHigh.freq;
+    bhPlanarCmb.dimord    = 'chan_freq_time';
+    
+elseif strcmp(freqRange, 'low')
+    % Do the same for low frequencies.
+    tll=[];
+    tll.avg    = betas_l;
+    tll.time   = tfaLow.time;
+    tll.dimord = 'subj_chan_time';
+    tll.label  = tfaLow.label;
+    tll.grad   = tfaLow.grad;
+    %
+    % % planar combination
+    cfg                 = [];
+    cfg.feedback        = 'no';
+    cfg.method          = 'template';
+    cfg.neighbours      = ft_prepare_neighbours(cfg, tll);
+    cfg.planarmethod    = 'sincos';
+    tllPlanar           = ft_megplanar(cfg, tll);
+    
+    cfg           = [];
+    blPlanarCmb  = ft_combineplanar(cfg,tllPlanar);
+    
+    % put it back in a freq-data structure
+    blPlanarCmb.powspctrm = permute(blPlanarCmb.trial, [2,1,3]);
+    blPlanarCmb           = rmfield(blPlanarCmb, 'trial');
+    blPlanarCmb.freq      = tfaLow.freq;
+    blPlanarCmb.dimord    = 'chan_freq_time';
+end
 %% Normalize beta weights
 % also doens't change much
-t1              = nearest(bhPlanarCmb.time, -0.6);
-t2              = nearest(bhPlanarCmb.time, -0.1);
-mu              = rmfield(bhPlanarCmb, 'powspctrm');
-mu.powspctrm    = mean(bhPlanarCmb.powspctrm(:,:,t1:t2), 3);
-mu.powspctrm    = repmat(mu.powspctrm, [1, 1, length(bhPlanarCmb.time)]);
-sigma           = rmfield(bhPlanarCmb, 'powspctrm');
-sigma.powspctrm = std(bhPlanarCmb.powspctrm(:,:,t1:t2),[],3);
-sigma.powspctrm = repmat(sigma.powspctrm, [1,1, length(bhPlanarCmb.time)]);
-
-cfg=[];
-cfg.parameter = 'powspctrm';
-cfg.operation = '(x1-x2)./x3';
-tlhPlanarCmbZ = ft_math(cfg, bhPlanarCmb, mu, sigma);
-
-% Do the same for low freq
-t1              = nearest(blPlanarCmb.time, -0.6);
-t2              = nearest(blPlanarCmb.time, -0.1);
-mu              = rmfield(blPlanarCmb, 'powspctrm');
-mu.powspctrm    = mean(blPlanarCmb.powspctrm(:,:,t1:t2), 3);
-mu.powspctrm    = repmat(mu.powspctrm, [1, 1, length(blPlanarCmb.time)]);
-sigma           = rmfield(blPlanarCmb, 'powspctrm');
-sigma.powspctrm = std(blPlanarCmb.powspctrm(:,:,t1:t2),[],3);
-sigma.powspctrm = repmat(sigma.powspctrm, [1,1, length(blPlanarCmb.time)]);
-
-cfg=[];
-cfg.parameter = 'powspctrm';
-cfg.operation = '(x1-x2)./x3';
-tllPlanarCmbZ = ft_math(cfg, blPlanarCmb, mu, sigma);
-
-%% Save
-if isPilot
-    filename = sprintf('/project/3011085.02/results/erf/pilot-%03d/glm_erf_tf', subj);
-else
-    filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_erf_tf', subj);
+if strcmp(freqRange, 'high')
+    t5              = nearest(bhPlanarCmb.time, -0.6);
+    t6              = nearest(bhPlanarCmb.time, -0.1);
+    muH              = rmfield(bhPlanarCmb, 'powspctrm');
+    muH.powspctrm    = nanmean(bhPlanarCmb.powspctrm(:,:,t5:t6), 3);
+    muH.powspctrm    = repmat(muH.powspctrm, [1, 1, length(bhPlanarCmb.time)]);
+    sigmaH           = rmfield(bhPlanarCmb, 'powspctrm');
+    sigmaH.powspctrm = nanstd(bhPlanarCmb.powspctrm(:,:,t5:t6),[],3);
+    sigmaH.powspctrm = repmat(sigmaH.powspctrm, [1,1, length(bhPlanarCmb.time)]);
+    
+    cfg=[];
+    cfg.parameter = 'powspctrm';
+    cfg.operation = '(x1-x2)./x3';
+    bhPlanarCmbZ = ft_math(cfg, bhPlanarCmb, muH, sigmaH);
+    
+elseif strcmp(freqRange, 'low')
+    % % Do the same for low freq
+    t7              = nearest(blPlanarCmb.time, -0.6);
+    t8              = nearest(blPlanarCmb.time, -0.1);
+    muL              = rmfield(blPlanarCmb, 'powspctrm');
+    muL.powspctrm    = mean(blPlanarCmb.powspctrm(:,:,t7:t8), 3);
+    muL.powspctrm    = repmat(muL.powspctrm, [1, 1, length(blPlanarCmb.time)]);
+    sigmaL           = rmfield(blPlanarCmb, 'powspctrm');
+    sigmaL.powspctrm = std(blPlanarCmb.powspctrm(:,:,t7:t8),[],3);
+    sigmaL.powspctrm = repmat(sigmaL.powspctrm, [1,1, length(blPlanarCmb.time)]);
+    
+    cfg=[];
+    cfg.parameter = 'powspctrm';
+    cfg.operation = '(x1-x2)./x3';
+    blPlanarCmbZ = ft_math(cfg, blPlanarCmb, muL, sigmaL);
 end
-save(fullfile([filename '.mat']), 'betas_l', 'blPlanarCmb','blPlanarCmbZ', 'betas_h','bhPlanarCmb', 'bhPlanarCmbZ','lat', '-v7.3');
+%% Save
+if strcmp(freqRange, 'high')
+    if isPilot
+        filename = sprintf('/project/3011085.02/results/erf/pilot-%03d/glm_tfH_%d', subj, zeropoint);
+    else
+        filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_tfH_%d', subj, zeropoint);
+    end
+    save(fullfile([filename '.mat']), 'betas_h','bhPlanarCmb', 'bhPlanarCmbZ','lat', '-v7.3');
+elseif strcmp(freqRange, 'low')
+    if isPilot
+        filename = sprintf('/project/3011085.02/results/erf/pilot-%03d/glm_tfL_%d', subj, zeropoint);
+    else
+        filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_tfL_%d', subj, zeropoint);
+    end
+    save(fullfile([filename '.mat']), 'betas_l', 'blPlanarCmb','blPlanarCmbZ', 'lat', '-v7.3');
+end
+
 diary off
 movefile(diaryname, fullfile([filename '.txt']));
 
