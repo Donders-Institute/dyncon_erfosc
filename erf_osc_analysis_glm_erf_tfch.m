ae7ca5ba1fd4cf29b94dbe8cd3e7c44b79da373e
Mats van Es <m.vanes@donders.ru.nl>
2017-07-14 16:24:27 +0200

    add update first changed in erf_osc_analysis_glm_erf_tfch_tmp.m. Changes include general cleanup, more efficient coding, representative p1 amplitude instead of channel specific, changes in variable names, and small bugfixes

--- erf_osc_analysis_glm_erf_tfch.m
+++ erf_osc_analysis_glm_erf_tfch.m
@@ -2,35 +2,23 @@ function erf_osc_analysis_glm_erf_tfch(subj, isPilot, freqRange, zeropoint, erfo
 % linear regression of peak amplitude over time-frequency (with fixed
 % channel) or over frequency-channel (with fixed (avg) time).
 
-if nargin<1
+if nargin<1 || isempty(subj)
     subj = 1;
 end
-if isempty(subj)
-    subj = 1;
-end
-if nargin<2
+if nargin<2 || isempty(isPilot)
     isPilot = false;
 end
-if isempty(isPilot);
-    isPilot = false;
-end
-if nargin<3
+if nargin<3 || isempty(freqRange)
     freqRange = 'high'; % can be 'high' or 'low'; depending on which frequency range you want to do regression (2-30 Hz or 28-100 Hz)
 end
-if isempty(freqRange)
-    freqRange = 'high';
-end
-if nargin<4
+if nargin<4 || isempty(zeropoint)
     zeropoint = 'onset'; % other option 'reversal': redefine time axis to stimulus reversal or keep it at stimulus onset
 end
-if isempty(zeropoint);
-    zeropoint = 'onset';
-end
-if nargin<5 % erf of interest
+if nargin<5 || isempty(erfoi) % erf of interest
     erfoi = 'reversal'; % other option 'onset': redefine time axis to stimulus reversal or keep it at stimulus onset
 end
-if isempty(erfoi);
-    erfoi = 'reversal';
+if nargin<6 || isempty(doDSS)
+  doDSS=false;
 end
 
 % Initiate Diary
@@ -40,9 +28,9 @@ ft_diary('on')
 erf_osc_datainfo;
 if isPilot
     load(sprintf('/project/3011085.02/results/erf/pilot-%03d/dss.mat', subj), 'data_dss');
-    load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_virtual_channel.mat', subj), 'gammaChan');
+    %load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_virtual_channel.mat', subj), 'gammaChan');
 else
-    load(sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%s.mat', subj, zeropoint));
+    %load(sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%s.mat', subj, zeropoint));
     if strcmp(erfoi, 'reversal')
         if doDSS
             [data, nComp_keep] = erf_osc_analysis_dss(subj,isPilot, 'reversal', false);
@@ -60,7 +48,6 @@ end
 %% if no data_dss cleaning is done beforehand, do this
 if ~doDSS
     data=dataClean;
-    cfg=[];
     idxM = find(data.trialinfo(:,5)>0 & data.trialinfo(:,6)>0);
     nTrials = length(idxM);
     
@@ -74,23 +61,26 @@ if ~doDSS
         cfg.offset = -(data.trialinfo(:,5)-data.trialinfo(:,4));
         data = ft_redefinetrial(cfg, data);
     end
+    clear dataClean;
 else
     data=data_dss;
+    clear data_dss;
 end
 
 fs=data.fsample;
 nTrials = length(data.trial);
 
 
-%% select p1 window for regression
+%% select p1 amplitude for regression
 cfg=[];
 cfg.vartrllength=2;
 tlck = ft_timelockanalysis(cfg, data);
+allchans = tlck.label;
 
 t1p1 = nearest(tlck.time, 0.06);
 t2p1 = nearest(tlck.time, 0.12);
 cfg=[];
-cfg.channel = {'MRO', 'MRP', 'MLO', 'MRO', 'MZO', 'MZP'};
+cfg.channel = {'MRO', 'MRP', 'MLO', 'MLP', 'MZO', 'MZP'};
 cfg.latency = [tlck.time(t1p1) tlck.time(t2p1)];
 tlck = ft_selectdata(cfg, tlck);
 
@@ -103,7 +93,7 @@ maxchanid = tlck.label(maxchan);
 maxchanidx = find(strcmp(maxchanid, data.label));
 halfwindowlength = 8;
 i=1;
-for ntrl = halfwindowlength+1:length(time)-halfwindowlength;
+for ntrl = halfwindowlength+1:length(time)-halfwindowlength
     win(i,:) = [time(ntrl-8), time(ntrl+8)];
     avg(i,1) = mean(tlck.avg(maxchan,ntrl-8:ntrl+8),2);
     i=i+1;
@@ -112,220 +102,113 @@ end
 lat = win(window,:);
 
 
-%% Regression p1 amplitude over time-frequency-channel
 cfg=[];
-cfg.latency = [-1.5 0.65];
+cfg.latency = lat;
+cfg.avgovertime = 'yes';
+cfg.channel = {'MRO', 'MRP', 'MLO', 'MLP', 'MZO', 'MZP'};
 data = ft_selectdata(cfg, data);
-trialdata = cat(3,data.trial{:});
 
-idxtime = [nearest(data.time{1}, lat(1)) : nearest(data.time{1}, lat(2))];
-p1amp = squeeze(mean(trialdata(:,idxtime,:),2));
+p1amp_all = cat(2,data.trial{:}); % posterior channels only
+p1amp_avgtrl = mean(p1amp_all,2); % average p1 amp over trials
+multiplier = sign(p1amp_avgtrl); % get the sign of the average
+% find a few representative posterior channels that have highest absolute mean amplitude over trials
+[~, idx] = sort(multiplier.*p1amp_avgtrl, 'descend'); 
+num_p1chans = 4;
+p1chans = idx(1:num_p1chans);
+p1chans_id = data.label(p1chans);
+% representative p1 amplitude is the mean of the *absolute* value of
+% representative channels. (Note that absolute here means sign-flipped if
+% a channel's average < 0);
+p1amp = multiplier(p1chans)'*p1amp_all(p1chans,:);
 
-% baselinecorrect with average baseline over trials
-if strcmp(freqRange, 'high');
-    if strcmp(zeropoint, 'onset')
-        cfg=[];
-        cfg.latency = [-1 1]; % shortest baseline window is 1 second, shortest reversal 1 second as well
-        tfaHigh = ft_selectdata(cfg, tfaHigh);
-    else
-        cfg=[];
-        cfg.latency = [-1.5 0];
-        tfaHigh = ft_selectdata(cfg, tfaHigh);
-    end
-    
-    for freq=1:19
-        for ch=1:length(data.label);
-            design = [ones(size(p1amp(ch,:))); p1amp(ch,:)];
-            design(2,:) = (design(2,:)-mean(design(2,:)))./std(design(2,:));
-            Y_h = squeeze(squeeze(tfaHigh.powspctrm(:,ch,freq,:)));
-            betas(freq,ch,:,:) = design'\Y_h;
-        end
-    end
-    
-    nchan = size(tfaHigh.powspctrm, 2);
-    nfreq = size(tfaHigh.powspctrm, 3);
-    ntime = size(tfaHigh.powspctrm, 4);
-    ntrl = size(p1amp,2);
-    design_bias = ones(1, ntrl);
-    p1amp_norm = (p1amp - repmat(mean(p1amp,2), [1 ntrl]))./repmat(std(p1amp, [], 2), [1, ntrl]);
-    
-    numShuffles=1000;
-    avg_shuffles = zeros(nchan, nfreq, ntime);
-    std_shuffles = zeros(nchan, nfreq, ntime);
-    for freq=1:19
-        Y1 = squeeze(tfaHigh.powspctrm(:,:,freq,:));
-        for ch=1:nchan;
-            Y2 = squeeze(Y1(:,ch,:));
-            design = [design_bias; p1amp_norm(ch,:)];
-            betas_shuffles=zeros(numShuffles, ntime);
-            for iShuffle=1:numShuffles
-                design2 = design(:,randperm(ntrl));
-                tmp = design2'\Y2;
-                betas_shuffles(iShuffle, :) = tmp(2,:);
-            end
-            avg_shuffles(ch, freq, :) = mean(betas_shuffles,1);
-            std_shuffles(ch, freq, :) = std(betas_shuffles,[],1);
-        end
-    end
-    
-elseif strcmp(freqRange, 'low')
-    if strcmp(zeropoint, 'onset')
-        cfg=[];
-        cfg.latency = [-1 1]; % shortest baseline window is 1 second
-        tfaLow = ft_selectdata(cfg, tfaLow);
-    else
-        cfg=[];
-        cfg.latency = [-1.5 0];
-        tfaLow = ft_selectdata(cfg, tfaLow);
-    end
-    
-    for freq=1:15
-        for ch=1:length(data.label);
-            design = [ones(size(p1amp(ch,:))); p1amp(ch,:)];
-            design(2,:) = (design(2,:)-mean(design(2,:)))./std(design(2,:));
-            Y_l = squeeze(squeeze(tfaLow.powspctrm(:,ch,freq,:)));
-            betas(freq,ch,:,:) = design'\Y_l;
-        end
-    end
-    
-    nchan = size(tfaLow.powspctrm, 2);
-    nfreq = size(tfaLow.powspctrm, 3);
-    ntime = size(tfaLow.powspctrm, 4);
-    ntrl = size(p1amp,2);
-    design_bias = ones(1, ntrl);
-    p1amp_norm = (p1amp - repmat(mean(p1amp,2), [1 ntrl]))./repmat(std(p1amp, [], 2), [1, ntrl]);
-    
-    numShuffles=1000;
-    avg_shuffles = zeros(nfreq, nchan, ntime);
-    std_shuffles = zeros(nfreq, nchan, ntime);
-    for freq=1:nfreq
-        Y1 = squeeze(tfaLow.powspctrm(:,:,freq,:));
-        for ch=1:nchan;
-            Y2 = squeeze(Y1(:,ch,:));
-            design = [design_bias; p1amp_norm(ch,:)];
-            betas_shuffles=zeros(numShuffles, ntime);
-            for iShuffle=1:numShuffles
-                design2 = design(:,randperm(ntrl));
-                tmp = design2'\Y2;
-                betas_shuffles(iShuffle, :) = tmp(2,:);
-            end
-            avg_shuffles(ch, freq, :) = mean(betas_shuffles,1);
-            std_shuffles(ch, freq, :) = std(betas_shuffles,[],1);
-        end
-    end
+%% Regression p1 amplitude over time-frequency-channel
+
+% load TFA data
+if isPilot
+  load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_virtual_channel.mat', subj), 'gammaChan');
+else
+  load(sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%s.mat', subj, zeropoint));
 end
-
-
-%% planar gradiant transformation of beta weights
-% make timelocked structure where planar gradient transformation can be
-% applied to (that's why dimord is strange)
 if strcmp(freqRange, 'high')
-    tl=[];
-    tl.avg    = squeeze(betas(:,:,2,:));
-    tl.time   = tfaHigh.time;
-    tl.dimord = 'subj_chan_time';
-    tl.label  = tfaHigh.label;
-    tl.grad   = tfaHigh.grad;
-    
-    % also put betas in a time-freq structure
-    tf = rmfield(tl,'avg');
-    tf.dimord = 'chan_freq_time';
-    tf.powspctrm = permute(tl.avg, [2,1,3]);
-    tf.freq = tfaHigh.freq;
-    
-    % planar combination
-    cfg                 = [];
-    cfg.feedback        = 'no';
-    cfg.method          = 'template';
-    cfg.neighbours      = ft_prepare_neighbours(cfg, tl);
-    cfg.planarmethod    = 'sincos';
-    tlPlanar           = ft_megplanar(cfg, tl);
-    
-    cfg           = [];
-    bPlanarCmb  = ft_combineplanar(cfg,tlPlanar);
-    
-    % put it back in a freq-data structure
-    bPlanarCmb.powspctrm = permute(bPlanarCmb.trial, [2,1,3]);
-    bPlanarCmb           = rmfield(bPlanarCmb, 'trial');
-    bPlanarCmb.freq      = tfaHigh.freq;
-    bPlanarCmb.dimord    = 'chan_freq_time';
-    
-    tl_shuffle_avg      = rmfield(tl, 'avg');
-    tl_shuffle_avg.avg  = permute(avg_shuffles, [2,1,3]);
-    tl_shuffle_std      = rmfield(tl, 'avg');
-    tl_shuffle_std.avg  = permute(std_shuffles,[2,1,3]);
-    
-    cfg                 = [];
-    cfg.feedback        = 'no';
-    cfg.method          = 'template';
-    cfg.neighbours      = ft_prepare_neighbours(cfg, tl_shuffle_avg);
-    cfg.planarmethod    = 'sincos';
-    tl_shuffle_avgPlanar= ft_megplanar(cfg, tl_shuffle_avg);
-    tl_shuffle_stdPlanar= ft_megplanar(cfg, tl_shuffle_std);
-    
-    cfg                      = [];
-    tl_shuffle_avgCmbPlanar  = ft_combineplanar(cfg,tl_shuffle_avgPlanar);
-    tl_shuffle_stdCmbPlanar  = ft_combineplanar(cfg,tl_shuffle_stdPlanar);
-    
-    shuffle_avgCmbPl = rmfield(bPlanarCmb, 'powspctrm');
-    shuffle_avgCmbPl.powspctrm = permute(tl_shuffle_avgCmbPlanar.trial, [2,1,3]);
-    shuffle_stdCmbPl = rmfield(bPlanarCmb, 'powspctrm');
-    shuffle_stdCmbPl.powspctrm = permute(tl_shuffle_stdCmbPlanar.trial, [2,1,3]);
-    
+  tfa = tfaHigh;
+  clear tfaHigh tfaLow; 
 elseif strcmp(freqRange, 'low')
-    % Do the same for low frequencies.
-    tl=[];
-    tl.avg    = squeeze(betas(:,:,2,:));
-    tl.time   = tfaLow.time;
-    tl.dimord = 'subj_chan_time';
-    tl.label  = tfaLow.label;
-    tl.grad   = tfaLow.grad;
-    
-    % also put betas in a time-freq structure
-    tf = rmfield(tl,'avg');
-    tf.dimord = 'chan_freq_time';
-    tf.powspctrm = permute(tl.avg, [2,1,3]);
-    tf.freq = tfaLow.freq;
-    
-    % planar combination
-    cfg                 = [];
-    cfg.feedback        = 'no';
-    cfg.method          = 'template';
-    cfg.neighbours      = ft_prepare_neighbours(cfg, tl);
-    cfg.planarmethod    = 'sincos';
-    tlPlanar           = ft_megplanar(cfg, tl);
-    
-    cfg           = [];
-    bPlanarCmb  = ft_combineplanar(cfg,tlPlanar);
-    
-    % put it back in a freq-data structure
-    bPlanarCmb.powspctrm = permute(bPlanarCmb.trial, [2,1,3]);
-    bPlanarCmb           = rmfield(bPlanarCmb, 'trial');
-    bPlanarCmb.freq      = tfaLow.freq;
-    bPlanarCmb.dimord    = 'chan_freq_time';
-    
-    tl_shuffle_avg      = rmfield(tl, 'avg');
-    tl_shuffle_avg.avg  = permute(avg_shuffles, [2,1,3]);
-    tl_shuffle_std      = rmfield(tl, 'avg');
-    tl_shuffle_std.avg  = permute(std_shuffles,[2,1,3]);
-    
-    cfg                 = [];
-    cfg.feedback        = 'no';
-    cfg.method          = 'template';
-    cfg.neighbours      = ft_prepare_neighbours(cfg, tl_shuffle_avg);
-    cfg.planarmethod    = 'sincos';
-    tl_shuffle_avgPlanar= ft_megplanar(cfg, tl_shuffle_avg);
-    tl_shuffle_stdPlanar= ft_megplanar(cfg, tl_shuffle_std);
-    
-    cfg                      = [];
-    tl_shuffle_avgCmbPlanar  = ft_combineplanar(cfg,tl_shuffle_avgPlanar);
-    tl_shuffle_stdCmbPlanar  = ft_combineplanar(cfg,tl_shuffle_stdPlanar);
-    
-    shuffle_avgCmbPl = rmfield(bPlanarCmb, 'powspctrm');
-    shuffle_avgCmbPl.powspctrm = permute(tl_shuffle_avgCmbPlanar.trial, [2,1,3]);
-    shuffle_stdCmbPl = rmfield(bPlanarCmb, 'powspctrm');
-    shuffle_stdCmbPl.powspctrm = permute(tl_shuffle_stdCmbPlanar.trial, [2,1,3]);
+  tfa = tfaLow;
+  clear tfaLow tfaHigh;
+else
+  error('freqRange should be specified as *low* (2-30Hz) or *high* (28-100 Hz)')
+  keyboard
 end
+
+% select active window of TFA
+if strcmp(zeropoint, 'onset')
+    cfg=[];
+    cfg.latency = [-1 1]; % shortest baseline window is 1 second, shortest reversal 1 second as well
+    tfa = ft_selectdata(cfg, tfa);
+else
+    cfg=[];
+    cfg.latency = [-1 0];
+    tfa = ft_selectdata(cfg, tfa);
+end
+
+nchan = size(tfa.powspctrm, 2);
+nfreq = size(tfa.powspctrm, 3);
+ntime = size(tfa.powspctrm, 4);
+ntrl = size(p1amp,2);
+
+design = [ones(size(p1amp)); ((p1amp-mean(p1amp))./std(p1amp))];
+for freq=1:nfreq
+    for ch=1:nchan
+        Y_h = squeeze(squeeze(tfa.powspctrm(:,ch,freq,:)));
+        betas(freq,ch,:,:) = design'\Y_h;
+    end
+end
+
+numShuffles=100;
+all_shuffles = zeros(nchan, nfreq, ntime, numShuffles);
+
+% prepare a data object and cfg for the planar transform
+tl=[];
+tl.avg    = squeeze(betas(:,:,2,:));
+tl.time   = tfa.time;
+tl.dimord = 'rpt_chan_time';
+tl.label  = tfa.label;
+tl.grad   = tfa.grad;
+
+cfg                 = [];
+cfg.feedback        = 'no';
+cfg.method          = 'template';
+cfg.neighbours      = ft_prepare_neighbours(cfg, tl);
+cfg.planarmethod    = 'sincos';
+tlPlCmb             = ft_combineplanar([], ft_megplanar(cfg, tl));
+
+for iShuffle = 1:numShuffles
+  shufvec = randperm(ntrl);  
+  betas_shuf = zeros(nchan, nfreq*ntime); % store betas for this shuffle across channels
+  for ch = 1:nchan
+    design_shuf = design;
+    design_shuf = design_shuf(:,shufvec);
+    Y1  = reshape(tfa.powspctrm(:,ch,:,:),[],nfreq*ntime);
+    tmp = design_shuf'\Y1;
+    betas_shuf(ch,:) = tmp(2,:);
+  end
+  tl.time = 1:nfreq*ntime;
+  tl.avg  = betas_shuf;
+  tmp     = ft_combineplanar([], ft_megplanar(cfg, tl));
+  betas_shuf = tmp.avg;
+  all_shuffles(:,:,:,iShuffle) = reshape(betas_shuf, [nchan nfreq ntime]);
+end
+avg_shuffles = mean(all_shuffles,4);
+std_shuffles = std(all_shuffles,[],4);
+
+%% Put betas and shuffles in freq-structure
+betasPlCmb           = rmfield(tfa, 'powspctrm');
+betasPlCmb.powspctrm = permute(tlPlCmb.trial, [2,1,3]);
+betasPlCmb.dimord    = 'chan_freq_time';
+shufflesAvgPlCmb     = rmfield(betasPlCmb, {'powspctrm', 'cfg'});
+shufflesAvgPlCmb.powspctrm = avg_shuffles;
+shufflesStdPlCmb     = rmfield(betasPlCmb, {'powspctrm', 'cfg'});
+shufflesStdPlCmb.powspctrm = std_shuffles;
 
 %% Save
 
@@ -334,10 +217,7 @@ if isPilot
 else
     filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_tf_%s_%s_erf_%s', subj, freqRange, zeropoint, erfoi);
 end
-
-save(fullfile([filename '.mat']), 'betas','bPlanarCmb','tf','lat','maxchanid','maxchanidx', 'shuffle_avgCmbPl', 'shuffle_stdCmbPl', 'p1amp', '-v7.3');
-
-
+save(fullfile([filename '.mat']), 'betas','betasPlCmb', 'all_shuffles', 'shufflesAvgPlCmb', 'shufflesStdPlCmb', 'lat', 'p1amp', 'maxchanid','p1chans_id', '-v7.3');
 
 ft_diary('off')
 
