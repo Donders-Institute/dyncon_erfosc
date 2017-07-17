2be7a6ef807f871de37d89b6db7e3e2614ff9b42
Mats van Es <m.vanes@donders.ru.nl>
2017-07-17 13:18:41 +0200

    make variable names non-specific. do planar gradient transformation of timedata before TFA

--- erf_osc_analysis_tfa.m
+++ erf_osc_analysis_tfa.m
@@ -1,4 +1,4 @@
-function erf_osc_analysis_tfa(subj, isPilot, zeropoint)
+function erf_osc_analysis_tfa(subj, isPilot, freqRange, zeropoint)
 %
 % trialinfo columns:
 % 1: trialnumber
@@ -7,23 +7,17 @@ function erf_osc_analysis_tfa(subj, isPilot, zeropoint)
 % 4: sample of grating onset
 % 5: sample of grating shift (=0 if no shift)
 % 6: sample of response (=0 if no response or if response too early)
-if nargin<1
+if nargin<1 || isempty(subj)
     subj = 1;
 end
-if isempty(subj)
-    subj = 1;
-end
-if nargin<2
+if nargin<2 || isempty(isPilot)
     isPilot = false;
 end
-if isempty(isPilot);
-    isPilot = false;
+if nargin<3 || isempty(freqRange)
+    freqRange = 'onset';
 end
-if nargin<3
-    zeropoint = 'onset'; % other option 'reversal': redefine time axis to stimulus reversal or keep it at stimulus onset
-end
-if isempty(zeropoint);
-    zeropoint = 'onset';
+if nargin<4 || isempty(zeropoint)
+    zeropoint = 'onset';% other option 'reversal': redefine time axis to stimulus reversal or keep it at stimulus onset
 end
 
 % initiate diary
@@ -61,35 +55,45 @@ cfg=[];
 cfg.latency = [-1.75 1.75];
 data = ft_selectdata(cfg, data);
 
+%% Planar transformation
+cfg                 = [];
+cfg.method          = 'template';
+cfg.template        = 'CTF275_neighb.mat';
+cfg.neighbours      = ft_prepare_neighbours(cfg, data);
+cfg.method          = 'sincos';
+data_planar         = ft_megplanar(cfg, data);
 
-%% TFA, low frequencies (2-30)
+%% TFA
+cfg                  = [];
+cfg.output           = 'pow';
+cfg.channel          = 'MEG';
+cfg.method           = 'mtmconvol';
+cfg.toi              = -1.75:0.05:1.75;
+cfg.keeptrials       = 'yes';
 
-cfg              = [];
-cfg.output       = 'pow';
-cfg.channel      = 'MEG';
-cfg.method       = 'mtmconvol';
-cfg.taper        = 'hanning';
-cfg.pad          = 6;
-cfg.foi          = 2:2:30;% analysis 2 to 30 Hz in steps of 2 Hz
-cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % length of time window = 0.5 sec
-cfg.toi          = -1.75:0.05:1.75;                  % time window "slides" from -0.5 to 1.5 sec in steps of 0.05 sec (50 ms)
-cfg.keeptrials   = 'yes';
-tfaLow = ft_freqanalysis(cfg, data);
+if strcmp(freqRange, 'low')
+    cfg.taper        = 'hanning';
+    cfg.pad          = 6;
+    cfg.foi          = 2:2:6%30;% analysis 2 to 30 Hz in steps of 2 Hz
+    cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % length of time window = 0.5 sec
+    
+elseif strcmp(freqRange, 'high')
+    cfg.taper        = 'dpss';
+    cfg.tapsmofrq    = 8; % 8 Hz freq smoothing on both sides
+    cfg.foi          = 28:4:36%100;
+    cfg.pad          = 6;
+    cfg.t_ftimwin    = ones(length(cfg.foi),1).*(1/4);
+    
+else
+    error('freqRange must be *high* or *low*')
+    keyboard
+end
 
-%% TFA, high frequencies
+tfa_planar           = ft_freqanalysis(cfg,data_planar);
 
-cfg              = [];
-cfg.output       = 'pow';
-cfg.channel      = 'MEG';
-cfg.method       = 'mtmconvol';
-cfg.taper        = 'dpss';
-cfg.tapsmofrq    = 8; % 8 Hz freq smoothing on both sides
-cfg.foi          = 28:4:100;
-cfg.pad          = 6;
-cfg.t_ftimwin    = ones(length(cfg.foi),1).*(1/4);
-cfg.toi          = -1.75:0.05:1.75;
-cfg.keeptrials   = 'yes';
-tfaHigh = ft_freqanalysis(cfg,data);
+cfg                  = [];
+cfg.method           = 'sum';
+tfa                  = ft_combineplanar(cfg,tfa_planar);
 
 %% get baseline
 if strcmp(zeropoint, 'onset')
@@ -97,27 +101,20 @@ if strcmp(zeropoint, 'onset')
     cfg.latency = [-1 -0.25];
     cfg.avgoverrpt = 'yes';
     cfg.avgovertime = 'yes';
-    baselineH = ft_selectdata(cfg, tfaHigh);
-    
-    cfg=[];
-    cfg.latency = [-1 -0.25];
-    cfg.avgoverrpt = 'yes';
-    cfg.avgovertime = 'yes';
-    baselineL = ft_selectdata(cfg, tfaLow);
+    baseline = ft_selectdata(cfg, tfa);
 end
-
 
 %% save
 
 if isPilot
-    filename = sprintf('/project/3011085.02/results/freq/pilot-%03d/tfa_%s', subj, zeropoint);
+    filename = sprintf('/project/3011085.02/results/freq/pilot-%03d/tfa_%s_%s', subj, freqRange, zeropoint);
 else
-    filename = sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%s', subj, zeropoint);
+    filename = sprintf('/project/3011085.02/results/freq/sub-%03d/tfa_%s_%s', subj, freqRange, zeropoint);
 end
 if strcmp(zeropoint, 'onset')
-    save(fullfile([filename '.mat']), 'tfaLow', 'tfaHigh', 'baselineL', 'baselineH');
+    save(fullfile([filename '.mat']), 'tfa', 'baseline');
 elseif strcmp(zeropoint, 'reversal')
-    save(fullfile([filename '.mat']), 'tfaLow', 'tfaHigh');
+    save(fullfile([filename '.mat']), 'tfa');
 end
 ft_diary('off')
 
