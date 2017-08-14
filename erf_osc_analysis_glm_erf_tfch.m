4895b95a40bfcf5792cc5ae738d761db927ece86
Mats van Es <m.vanes@donders.ru.nl>
2017-08-14 14:03:46 +0200

    also save baseline betas (zeropoint = onset), in case the statistical comparisson is active vs. baseline instead of active vs. shuffles

--- erf_osc_analysis_glm_erf_tfch.m
+++ erf_osc_analysis_glm_erf_tfch.m
@@ -145,8 +145,12 @@ ntrl = size(p1amp,2);
 design = [ones(size(p1amp)); ((p1amp-mean(p1amp))./std(p1amp))];
 for freq=1:nfreq
     for ch=1:nchan
-        Y_h = squeeze(squeeze(tfa.powspctrm(:,ch,freq,:)));
-        betas_tmp(freq,ch,:,:) = design'\Y_h;
+        Y = squeeze(squeeze(tfa.powspctrm(:,ch,freq,:)));
+        betas_tmp(freq,ch,:,:) = design'\Y;
+        if strcmp(zeropoint, 'onset')
+            Y_bl = squeeze(squeeze(baseline.powspctrm(:,ch,freq,:)));
+            betas_bl_tmp(freq,ch,:,:) = design'\Y_bl;
+        end
     end
 end
 
@@ -171,6 +175,12 @@ std_shuffles = std(all_shuffles,[],4);
 betas = rmfield(tfa, {'powspctrm', 'cfg'});
 betas.powspctrm = permute(squeeze(betas_tmp(:,:,2,:)), [2,1,3]);
 betas.dimord = 'chan_freq_time';
+if strcmp(zeropoint, 'onset')
+    betas_baseline = rmfield(betas, 'powspctrm');
+    betas_baseline.powspctrm = permute(squeeze(betas_bl_tmp(:,:,2,:)), [2,1,3]);
+else
+    betas_baseline = 'see zeropoint=onset';
+end
 shufflesAvg     = rmfield(betas, 'powspctrm');
 shufflesAvg.powspctrm = avg_shuffles;
 shufflesStd     = rmfield(betas, 'powspctrm');
@@ -183,7 +193,7 @@ if isPilot
 else
     filename = sprintf('/project/3011085.02/results/erf/sub-%03d/glm_tf_%s_%s_erf_%s', subj, freqRange, zeropoint, erfoi);
 end
-save(fullfile([filename '.mat']), 'betas', 'all_shuffles', 'shufflesAvg', 'shufflesStd', 'lat', 'p1amp', 'maxchanid','p1chans_id', '-v7.3');
+save(fullfile([filename '.mat']), 'betas','betas_baseline', 'all_shuffles', 'shufflesAvg', 'shufflesStd', 'lat', 'p1amp', 'maxchanid','p1chans_id', '-v7.3');
 
 ft_diary('off')
 
