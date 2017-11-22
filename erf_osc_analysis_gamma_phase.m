dd0352c08e062f4dbfb3835205c17a61ce5bcb78
Mats van Es <m.vanes@donders.ru.nl>
2017-11-22 16:00:48 +0100

    Use inter trial phase coherence to determine moment of signal input. Estimate gammma phase at this latency

--- erf_osc_analysis_gamma_phase.m
+++ erf_osc_analysis_gamma_phase.m
@@ -1,84 +1,112 @@
-function erf_osc_analysis_gamma_phase(subj, isPilot)
+function erf_osc_analysis_gamma_phase(subj, erfoi, filtdir)
 
-if nargin<1
-    subj = 3;
+if nargin<1 || isempty(subj)
+    subj = 1;
 end
-if isempty(subj)
-    subj = 3;
+if nargin<2 || isempty(erfoi)
+    erfoi = 'reversal';
 end
-if nargin<2
-    isPilot = true;
-end
-if isempty(isPilot);
-    isPilot = true;
+if nargin<3 && strcmp(erfoi, 'reversal') || isempty(erfoi) && strcmp(erfoi, 'reversal')
+    filtdir = 'reverse';
+else filtdir=[];
 end
 
 %% initiate diary
-workSpace = whos;
-diaryname = tempname(fullfile([getenv('HOME'), '/tmp']));
-diary(diaryname) % save command window output
-fname = mfilename('fullpath')
-datetime
-
-fid = fopen(fullfile([fname '.m']));
-tline = fgets(fid); % returns first line of fid
-while ischar(tline) % at the end of the script tline=-1
-    disp(tline) % display tline
-    tline = fgets(fid); % returns the next line of fid
-end
-fclose(fid);
-
-for i = 1:numel(workSpace) % list all workspace variables
-    workSpace(i).name % list the variable name
-    printstruct(eval(workSpace(i).name)) % show its value(s)
-end
+% ft_diary('on')
 
 %% load data
 erf_osc_datainfo;
-if isPilot
-    load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_virtual_channel.mat', subj), 'gamPowData');
-    load(sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_peak', subj), 'peakFreq');
-else
-    load(sprintf('/project/3011085.02/results/freq/sub-0%d/gamma_virtual_channel.mat', subj), 'gamPowData');
-    load(sprintf('/project/3011085.02/results/freq/sub-0%d/gamma_peak', subj), 'peakFreq');
-end
-fs = gamPowData.fsample;
-nTrials = length(gamPowData.trial);
+load(sprintf('/project/3011085.02/results/freq/sub-%03d/gamma_virtual_channel.mat', subj), 'lcmvData');
+load(sprintf('/project/3011085.02/results/freq/sub-%03d/pow.mat', subj), 'peakFreq_gamma');
 
-cfg                = [];
-cfg.offset         = -(gamPowData.trialinfo(:,5)-gamPowData.trialinfo(:,4));
-gamPowDataShift    = ft_redefinetrial(cfg, gamPowData);
+data = erf_osc_analysis_lcmv_orientation(subj, erfoi, filtdir); % optimal dipole orientation is chosen based on [0 0.5] after stimulus-reversal.
+% data is timelocked to reversal or behavioral response, and low pass
+% filtered. 
 
-cfg          = [];
-cfg.latency  = [-5/peakFreq 0-1/fs]; % 5 cycles of peak freq
-dataPreShift = ft_selectdata(cfg, gamPowDataShift);
+fs=data.fsample;
+%% ITC
+cfg              = [];
+cfg.output       = 'fourier';
+cfg.method       = 'mtmconvol';
+cfg.taper        = 'hanning';
+cfg.pad          = 6;
+cfg.foi          = 1:1:24;% analysis 2 to 30 Hz in steps of 2 Hz 
+cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.1;   % length of time window
+cfg.toi          = -1:1/fs:0.6;                  % time window "slides" in steps of 1/fs
+freq = ft_freqanalysis(cfg, data);
+
+% make a new FieldTrip-style data structure containing the ITC
+% copy the descriptive fields over from the frequency decomposition
+itc = [];
+itc.label     = freq.label;
+itc.freq      = freq.freq;
+itc.time      = freq.time;
+itc.dimord    = 'chan_freq_time';
+
+F = freq.fourierspctrm;   % copy the Fourier spectrum
+N = size(F,1);           % number of trials
+
+% compute inter-trial phase coherence (itpc) 
+itc.itpc      = F./abs(F);         % divide by amplitude  
+itc.itpc      = sum(itc.itpc,1);   % sum angles
+itc.itpc      = abs(itc.itpc)/N;   % take the absolute value and normalize
+itc.itpc      = squeeze(itc.itpc); % remove the first singleton dimension
+
+% compute inter-trial linear coherence (itlc)
+itc.itlc      = sum(F) ./ (sqrt(N*sum(abs(F).^2)));
+itc.itlc      = abs(itc.itlc);     % take the absolute value, i.e. ignore phase
+itc.itlc      = squeeze(itc.itlc); % remove the first singleton dimension
+
+figure;
+plot(itc.time, mean(itc.itpc));
+title(sprintf('subject %d', subj));
+
 
 %% estimate gamma angle
+cfg=[];
+cfg.latency = [-0.25 0.035];
+dataShort = ft_selectdata(cfg, data);
+
 cfg            = [];
 cfg.taper      = 'hanning';
 cfg.output     = 'fourier';
 cfg.method     = 'mtmfft';
 cfg.keeptrials = 'yes';
-cfg.tapsmofrq  = 5;
-cfg.foilim     = [peakFreq peakFreq];
-fcomp          = ft_freqanalysis(cfg, dataPreShift);
-gamAngle       = angle(fcomp.fourierspctrm(:,1,:)); % in radians
-% gamAngle       = radtodeg(gamAngle)+180; % shift to 0-360 degree
+cfg.tapsmofrq  = 8;
+cfg.foilim     = [peakFreq_gamma peakFreq_gamma];
+fcomp          = ft_freqanalysis(cfg, dataShort);
+gamAngle       = angle(fcomp.fourierspctrm); % in radians
 
-% bins = 0:60:360;
-% angleBin = zeros(nTrials,1);
-% for iTrl = 1:nTrials
-% angleBin(iTrl) = bins(nearest(bins, gamAngle(iTrl)));
-% end
-% angleBin(angleBin==0) = 360;
+[idx_peak, ~] = find(gamAngle>pi/4 & gamAngle<3*pi/4);
+[idx_through, ~] = find(gamAngle>-3*pi/4 & gamAngle<-pi/4);
+[idx_up, ~] = find(gamAngle>-pi/4 & gamAngle<pi/4);
+[idx_down, ~] = find(gamAngle>3*pi/4 | gamAngle<-3*pi/4);
 
+N = min([length(idx_peak), length(idx_through), length(idx_up), length(idx_down)]);
+
+cfg=[];
+cfg.trials = idx_peak(1:N);
+cfg.preproc.demean='yes';
+cfg.preproc.baselinewindow = [-0.1 0];
+peak = ft_selectdata(cfg, data);
+cfg.trials = idx_through(1:N);
+through = ft_selectdata(cfg, data);
+cfg.trials = idx_up(1:N);
+up = ft_selectdata(cfg, data);
+cfg.trials = idx_down(1:N);
+down = ft_selectdata(cfg, data);
+
+figure;
+ft_singleplotER([], up, peak, down, through);
+xlim([0 0.4])
+legend({'up', 'peak', 'down', 'through'})
 %% save
-if isPilot
-    filename = sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_angle', subj);
-else
-    filename = sprintf('/project/3011085.02/results/freq/sub-%03d/gamma_angle', subj);
-end
-save(fullfile([filename '.mat']), 'gamAngle');
-diary off
-movefile(diaryname, fullfile([filename '.txt']));
+% if isPilot
+%     filename = sprintf('/project/3011085.02/results/freq/pilot-%03d/gamma_angle', subj);
+% else
+%     filename = sprintf('/project/3011085.02/results/freq/sub-%03d/gamma_angle', subj);
+% end
+% save(fullfile([filename '.mat']), 'gamAngle');
+% ft_diary('off')
+
 end
