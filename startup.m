3526dfa44f228a83e20b4a3d6dc75cf49be86604
Mats van Es <m.vanes@donders.ru.nl>
2018-09-12 11:29:46 +0200

    added paths of external toolboxes

--- startup.m
+++ startup.m
@@ -19,7 +19,11 @@ else
     addpath('/home/electromag/matves/MATLAB/dss_aseo/peakfit');
     addpath('/home/electromag/matves/MATLAB/dss_aseo/ASEO');
     addpath('/home/electromag/matves/MATLAB/dss_aseo/dss2_1-0');
-   
+    
+    % Canlab Modulation toolbox
+    addpath(genpath('/project/3011085.02/scripts/ModulationToolbox/'));
+    addpath(genpath('/project/3011085.02/scripts/CanlabCore/'));
+    
     cd('/project/3011085.02/scripts/erfosc');
 end
 ft_defaults
