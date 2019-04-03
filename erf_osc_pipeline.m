7b26cec009865dafe19cd8cbe5cd6f4907172f56
Mats van Es <m.vanes@donders.ru.nl>
2019-04-03 16:52:10 +0200

    bugfixes and improvements.

--- erf_osc_pipeline.m
+++ erf_osc_pipeline.m
@@ -4,26 +4,9 @@ startup
 
 erf_osc_datainfo; % get subject specific information
 
+dosave = false;
+
 %% preprocessing
-% preprocessing is done semi-automatically; bad channels/trials,
-% jump/muscle and eye blink artifacts have to be manually verified.
-% ICA decomposition should be inspected and components to be deleted should
-% be specified in erf_osc_datainfo, under subjects(subj).ecgcomp/eyecomp
-% before continuing. This function saves the multiple files: one containing
-% sample information on artifacts, one with ICA spatial filters, one with
-% headmotion data and one with the preprocessed MEG data.
-for subj=allsubs
-    erf_osc_preprocessing_artifact(subj, false, false, 5)
-end
-
-% seperately, preprocess the eyetracker data. Transform X- and Y-gaze
-% positions to visual degrees relative to fixation. Save it on disk
-% together with pupil diameter data, both for data time locked to stimulus
-% onset and stimulus change.
-for subj=allsubs
-    erf_osc_preprocessing_eyedata(subj);
-end
-
 % seperately preprocess the MRI data using fieldtrip, freesurfer and
 % workbench. Do coregistration, segmentation and create 2D and 3D
 % sourcemodels and a headmodel. This script cannot be automatized and
@@ -34,84 +17,73 @@ end
 
 
 
-%% Analysis
-%% Reaction times
+% MEG preprocessing is done semi-automatically; bad channels/trials,
+% jump/muscle and eye blink artifacts have to be manually verified.
+% ICA decomposition should be inspected and components to be deleted should
+% be specified in erf_osc_datainfo, under subjects(subj).ecgcomp/eyecomp
+% before continuing. This function saves the multiple files: one containing
+% sample information on artifacts, one with ICA spatial filters, one with
+% headmotion data and one with the preprocessed MEG data.
+    global ft_default;
+    ft_default = [];
+    ft_default.reproducescript = datestr(now, 30);
 for subj=allsubs
-    erf_osc_analysis_rt(subj);
-end
+    erf_osc_preprocessing_artifact(subj, false, true, 5, dosave)
 
+% seperately, preprocess the eyetracker data. Transform X- and Y-gaze
+% positions to visual degrees relative to fixation. Save it on disk
+% together with pupil diameter data, both for data time locked to stimulus
+% onset and stimulus change.
 
-%% Stimulus induced effects
-for subj=allsubs
-    erf_osc_analysis_tfa(subj, false, 'low', 'onset') % low frequencies
-    erf_osc_analysis_tfa(subj, false, 'high', 'onset') % high frequencies
-end
+    [eyedata_onset, eyedata_shift] = erfosc_preprocessing_eyedata(subj, dosave);
 
-% combine them for all subjects and make a relative contrast with the
-% average baseline
-erf_osc_analysis_tfa_allsubs('low', 'onset');
-erf_osc_analysis_tfa_allsubs('high', 'onset');
+    %% Single Subject Analysis
+% Reaction times
+    erf_osc_analysis_rt(subj, dosave);
 
+% Stimulus induced effects
+
+    erf_osc_analysis_tfa(subj, false, 'low', 'onset', dosave) % low frequencies
+    erf_osc_analysis_tfa(subj, false, 'high', 'onset', dosave) % high frequencies
 
 %% power spectra
 % compute channel level induced power spectra in low and high frequencies.
-for subj=allsubs
     erf_osc_analysis_pow(subj, false);
+
+%% 2D source parcels    
+    erfosc_execute_pipeline('erfosc_script', subj, {'dolcmv_parc', true}, {'dolcmv_norm', true}, {'savefreq', dosave});
+    
+%% Source power contrast
+    erfosc_execute_pipeline('erfosc_script', subj, {'dodics_gamma', true}, {'dodics_lowfreq', true}, {'savefreq', dosave});
+
+%% correlation of rt with gamma in 3D sourcespace
+    erfosc_execute_pipeline('erfosc_script', subj, {'docorr_gamma_rt', true}, {'savefreq', dosave});
+
+%% Correlation Gamma ERF
+    erfosc_execute_pipeline('erfosc_script', subj, {'docorrpow_lcmv',1}, {'dofreq_short', 1}, {'savefreq', dosave}, {'dolcmv_norm', 1});
+    erfosc_execute_pipeline('erfosc_script', subj, {'docorrpow_lcmv_lowfreq',1}, {'dofreq_short_lowfreq', 1}, {'savefreq', dosave});
+    ft_default.reproducescript = [];
 end
 
-%% Source contrast
-for subj=allsubs
-    erfosc_execute_pipeline('erfosc_script_jm', subj, {'dodics_gamma', true}, {'dodics_lowfreq', true});
-end
+%% Group Analysis
+%% Induced power
+erf_osc_analysis_tfa_allsubs('low', 'onset', dosave);
+erf_osc_analysis_tfa_allsubs('high', 'onset', dosave);
 
 %% Correlation with RT
 % computing correlations and partial correlations between jitter, gamma
 % power and rt.
 erf_osc_analysis_corr([],[],'gamma_rt')
 
-% correlation of rt with gamma in 3D sourcespace
-for subj=allsubs
-    erfosc_execute_pipeline('erfosc_script_jm', subj, {'docorr_gamma_rt', true});
-end
-
 %% Correlation Gamma ERF
-for subj=allsubs
-    erfosc_execute_pipeline('erfosc_script_jm', subj, {'docorrpow_lcmv',1}, {'dofreq_short', 1});
-    erfosc_execute_pipeline('erfosc_script_jm', subj, {'docorrpow_lcmv_lowfreq',1}, {'dofreq_short_lowfreq', 1});
-end
-
-% do group statistics
-erfosc_execute_pipeline('erfosc_script_jm', 1, {'dostat_pow_erf',1}, {'GA', 1}, {'whichFreq', 1});
-erfosc_execute_pipeline('erfosc_script_jm', 1, {'dostat_pow_erf',1}, {'GA', 1}, {'whichFreq', 2});
+erfosc_execute_pipeline('erfosc_script', 1, {'dostat_pow_erf',1}, {'GA', 1}, {'whichFreq', 1}, {'savefreq', dosave});
+erfosc_execute_pipeline('erfosc_script', 1, {'dostat_pow_erf',1}, {'GA', 1}, {'whichFreq', 2},, {'savefreq', dosave});
 
 
 %% Correlation ERF RT
-erfosc_execute_pipeline('erfosc_script_jm', 1, {dostat_erf_rt, 'true'});
-
-
-
+erfosc_execute_pipeline('erfosc_script', 1, {dostat_erf_rt, 'true'});
 
 %% Plotting
 plot_figures_chapter
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ft_default.reproducescript = [];
